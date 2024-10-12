a4f6c3c4ec61af0206a15f62469d221ec46265e1
novsource <evstafev.nik@yandex.ru>
2024-10-12 16:54:29 +0800

    feat(styles): add colors

--- apps/streamers-dashboard/tailwind.config.js
+++ apps/streamers-dashboard/tailwind.config.js
@@ -2,6 +2,30 @@
 export default {
   content: ['./src/**/*.{html,js,ts,tsx,jsx}'],
   theme: {
+    colors: {
+      white: '#FFF',
+      green: {
+        DEFAULT: '#74DFA2',
+      },
+      dark: {
+        DEFAULT: '#27272A',
+        accent: '#3E4145',
+        foreground: '#18181B',
+      },
+      yellow: {
+        DEFAULT: '#CFC56F',
+      },
+      red: {
+        DEFAULT: '#CF6F6F',
+      },
+      gray: {
+        DEFAULT: '#555',
+        accent: '#AAA',
+      },
+      purple: {
+        DEFAULT: '#505285',
+      },
+    },
     extend: {},
   },
   plugins: [],
