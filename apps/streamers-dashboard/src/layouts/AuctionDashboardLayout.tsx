8f120161444439a851398f9be3d10de97696e16b
novsource <evstafev.nik@yandex.ru>
2024-10-13 15:06:44 +0800

    feat(ui): add Header

--- apps/streamers-dashboard/src/layouts/AuctionDashboardLayout.tsx
+++ apps/streamers-dashboard/src/layouts/AuctionDashboardLayout.tsx
@@ -1,9 +1,13 @@
 import {PropsWithChildren} from 'react';
+import Header from '../shared/components/ui/Header/Header';
 
 const AuctionDashboardLayout = ({children}: PropsWithChildren) => {
   return (
     <>
-      <main>{children}</main>
+      <Header />
+      <main className="h-full w-full">
+        <div className="h-full w-full px-4 mt-4">{children}</div>
+      </main>
     </>
   );
 };
---
+++ apps/streamers-dashboard/src/shared/components/ui/Header/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="w-full">
+      <div className="w-full px-4 pt-2">
+        <div className="flex w-full justify-between items-center gap-x-4">
+          <div className="">Logo</div>
+          <div className="">Logo</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
