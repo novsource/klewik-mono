eedceebc22ac78903deeb1d2393ead2feec9037c
novsource <evstafev.nik@yandex.ru>
2024-10-13 21:52:39 +0800

    feat(utils): add canvas utils

---
+++ apps/streamers-dashboard/src/shared/utils/canvas.ts
@@ -0,0 +1,249 @@
+export const getMaxSizeCanvas = (wrapper: HTMLDivElement): number => {
+  const wrapperWidth = wrapper.offsetWidth;
+  const wrapperHeight = wrapper.offsetHeight;
+
+  const isMobile = window.innerWidth <= 1024;
+  const wrapperPosY = wrapper.getBoundingClientRect().y;
+
+  const generalMaxHeight = isMobile
+    ? window.innerHeight - 56 - wrapperPosY
+    : window.innerHeight - wrapperPosY;
+
+  const targetSize = Math.min(wrapperWidth, wrapperHeight, generalMaxHeight);
+
+  return targetSize;
+};
+
+export const getCenterCanvas = (
+  canvas: HTMLCanvasElement
+): {x: number; y: number} => {
+  return {x: canvas.width / 2, y: canvas.height / 2};
+};
+
+export const resizeCanvas: ResizeCanvas = ({
+  canvas,
+  wheelSelector,
+  wrapper,
+}) => {
+  const size = wrapper.getBoundingClientRect().width;
+  const ratio = window.devicePixelRatio || 1;
+
+  canvas.width =
+    canvas.height =
+    wheelSelector.width =
+    wheelSelector.height =
+      Math.floor(size * ratio);
+
+  canvas.style.width = wheelSelector.style.width = `${size}px`;
+  canvas.style.height = wheelSelector.style.height = `${size}px`;
+};
+
+export const drawSlice: DrawSlice = ({
+  context: ctx,
+  options,
+  onDraw,
+  sliceParameters: {x, y, radius, startAngle, endAngle},
+}) => {
+  const slice = new Path2D();
+
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 2;
+
+  ctx.save();
+
+  // ctx.fillStyle = getRandomRGBColor();
+  ctx.fillStyle = options?.color ?? getRandomHSLColor();
+
+  // Draw slice
+  ctx.beginPath();
+
+  ctx.moveTo(x, y);
+  slice.moveTo(x, y);
+
+  ctx.arc(x, y, radius - ctx.lineWidth, startAngle, endAngle);
+
+  slice.arc(x, y, radius - ctx.lineWidth, startAngle, endAngle);
+
+  ctx.closePath();
+
+  slice.closePath();
+
+  ctx.fill();
+
+  ctx.moveTo(x, y);
+  slice.moveTo(x, y);
+
+  ctx.stroke();
+
+  ctx.save();
+
+  // Draw text
+
+  const angle = convertRadiansToDegrees(endAngle - startAngle);
+  const arcLength = getArcLength(angle, radius);
+
+  // const arcPercentToMainArc = getPercentValue(mainArcLength, arcLength) * 100;
+
+  if (options?.text) {
+    const metrics = ctx.measureText(options.text);
+    const textHeight =
+      metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    if (textHeight < arcLength * 0.125)
+      drawTextOnSlice(ctx, options?.text ?? 'untitled', {
+        x,
+        y,
+        angle: (endAngle + startAngle) / 2,
+      });
+  }
+
+  if (onDraw !== undefined) {
+    onDraw(slice);
+  }
+
+  ctx.restore();
+};
+
+export const drawTextOnSlice = (
+  ctx: CanvasRenderingContext2D,
+  text: string,
+  slice: {x: number; y: number; angle: number}
+) => {
+  ctx.fillStyle = 'white';
+  ctx.textAlign = 'center';
+  const radius = slice.x;
+
+  const fitText = fitTextEllipsis(ctx, text, radius * 0.25);
+  setFontSizeForCanvasText(ctx, radius, fitText);
+
+  const metrics = ctx.measureText(text);
+  const textHeight =
+    metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+  ctx.translate(slice.x, slice.y);
+  ctx.rotate(slice.angle);
+  ctx.fillText(fitText, slice.x * 0.65, textHeight / 2 - ctx.lineWidth);
+};
+
+const fitText = ({
+  ctx,
+  text,
+  maxWidth,
+  separator,
+}: {
+  ctx: CanvasRenderingContext2D;
+  text: string;
+  maxWidth: number;
+  separator: string;
+}) => {
+  let result = '';
+
+  for (let charIndex = 0; charIndex < text.length - 1; charIndex++) {
+    const metrics = ctx.measureText(result + separator);
+
+    if (metrics.width > maxWidth) {
+      return [...result, ...separator].join('');
+    }
+
+    result += text[charIndex];
+  }
+
+  return text;
+};
+
+export const wrapText = ({
+  ctx,
+  text,
+  maxWidth,
+  separator = '-',
+}: {
+  ctx: CanvasRenderingContext2D;
+  text: string;
+  maxWidth: number;
+  separator: string;
+}) => {
+  const result = [];
+
+  let str = '';
+
+  for (let charIndex = 0; charIndex <= text.length - 1; charIndex++) {
+    const metrics = ctx.measureText(str + separator + '\n');
+
+    if (metrics.width >= maxWidth) {
+      result.push(str + separator + '\n');
+      text = text.slice(charIndex, text.length);
+      charIndex = 0;
+      str = separator;
+    }
+
+    str += text[charIndex];
+  }
+
+  if (str.length !== 0) result.push(str);
+
+  return result;
+};
+
+export const fitTextEllipsis = (
+  ctx: CanvasRenderingContext2D,
+  text: string,
+  maxWidth: number
+) => {
+  return fitText({ctx, text, maxWidth, separator: '...'});
+};
+
+export const clearCanvas = (canvas: HTMLCanvasElement) => {
+  const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+export const setFontSizeForCanvasText = (
+  ctx: CanvasRenderingContext2D,
+  radius: number,
+  text: string
+) => {
+  const defaultFontSizeRem = 30;
+
+  for (let i = defaultFontSizeRem; i > 20; i -= 0.15) {
+    ctx.font = `${i}px sans-serif`;
+
+    const measureText = ctx.measureText(text);
+
+    if (measureText.width < radius * 0.2) return;
+  }
+};
+
+export const convertDegreesToRadians = (degrees: number) => {
+  return degrees * (Math.PI / 180);
+};
+
+export const convertRadiansToDegrees = (radians: number) => {
+  return radians * (180 / Math.PI);
+};
+
+export const getDegreeByArcLength = (radius: number, arcLength: number) => {
+  return (180 * arcLength) / (Math.PI * radius);
+};
+
+export const getMaxCircleLength = (radius: number) => {
+  return radius * 2 * Math.PI;
+};
+
+export const getArcLength = (angle: number, radius: number) => {
+  return (angle * Math.PI * radius) / 180;
+};
+
+export const getPercentValue = (numTarget: number, num: number) => {
+  return num / numTarget;
+};
+
+export const getRandomRGBColor = () => {
+  return `rgb(${Math.random() * 255},${Math.random() * 255},${
+    Math.random() * 255
+  })`;
+};
+
+export const getRandomHSLColor = () => {
+  return 'hsl(' + 360 * Math.random() + ',40%,48%)';
+};
---
+++ apps/streamers-dashboard/src/shared/utils/wheelCanvas.ts
@@ -0,0 +1,483 @@
+import {
+  clearCanvas,
+  convertDegreesToRadians,
+  drawSlice,
+  getDegreeByArcLength,
+  getMaxCircleLength,
+  getPercentValue,
+  getRandomHSLColor,
+} from './canvas';
+
+export const drawEmptyWheel = (canvas: HTMLCanvasElement) => {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const radius = canvas.width / 2;
+
+  const center = radius;
+
+  ctx.fillStyle = getRandomHSLColor();
+
+  ctx.save();
+
+  ctx.beginPath();
+  // ctx.moveTo(center, center);
+  ctx.arc(center, center, radius, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.clip();
+
+  ctx.fill();
+
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 3;
+
+  ctx.stroke();
+
+  ctx.restore();
+};
+
+export const drawSlicesItems = (
+  canvas: HTMLCanvasElement,
+  items: AuctionSlot[]
+) => {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const radius = canvas.width / 2;
+
+  const x = canvas.width / 2;
+  const y = canvas.height / 2;
+
+  let startAngle = 0;
+  const sumValues = items.reduce((acc, item) => acc + item['value'], 0);
+
+  for (const item of items) {
+    const sliceArcLength =
+      getMaxCircleLength(radius) * getPercentValue(sumValues, item.value);
+    const endAngle =
+      startAngle +
+      convertDegreesToRadians(getDegreeByArcLength(radius, sliceArcLength));
+
+    drawSlice({
+      context: ctx,
+      sliceParameters: {x, y, radius, startAngle, endAngle},
+      options: {text: item.name, color: item.auctionColor},
+    });
+
+    startAngle = endAngle;
+  }
+};
+
+export const drawSelector = (
+  selectorCanvas: HTMLCanvasElement,
+  wheelImageSize: number = 0.2
+) => {
+  const ctx = selectorCanvas.getContext('2d') as CanvasRenderingContext2D;
+
+  const selectorHeight = selectorCanvas.width / 50;
+  const center = selectorCanvas.width / 2;
+  const radius = selectorCanvas.width / 2;
+
+  const startAngle =
+    window.devicePixelRatio <= 1
+      ? convertDegreesToRadians(255)
+      : convertDegreesToRadians(245);
+  const endAngle =
+    window.devicePixelRatio <= 1
+      ? convertDegreesToRadians(285)
+      : convertDegreesToRadians(295);
+
+  ctx.strokeStyle = 'white';
+  ctx.fillStyle = '#283345';
+  ctx.lineWidth = 2;
+
+  ctx.save();
+
+  // Draw circle those wrapping image in wheel center
+
+  ctx.beginPath();
+
+  ctx.moveTo(center, center);
+  ctx.arc(center, center, radius * wheelImageSize, 0, 2 * Math.PI * radius);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.restore();
+
+  // Draw selector
+
+  const selectorX = center;
+  const selectorY = center - (selectorCanvas.width * wheelImageSize) / 2;
+
+  ctx.beginPath();
+
+  ctx.moveTo(selectorX, selectorY - selectorHeight);
+
+  ctx.arc(
+    center,
+    center,
+    (selectorCanvas.width * wheelImageSize) / 2,
+    startAngle,
+    endAngle
+  );
+
+  ctx.fill();
+
+  ctx.closePath();
+
+  ctx.stroke();
+
+  ctx.restore();
+};
+
+export const drawWheelOverlay = (canvas: HTMLCanvasElement) => {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  const center = canvas.width / 2;
+
+  ctx.fillStyle = 'rgba(0,0,0,0.65)';
+
+  ctx.save();
+
+  ctx.moveTo(center, center);
+  ctx.arc(center, center, center, 0, Math.PI * 2 * canvas.width);
+  ctx.closePath();
+  ctx.fill();
+};
+
+export const drawSlicesAsPath2D = (
+  canvas: HTMLCanvasElement,
+  items: AuctionSlotWithAngles[]
+) => {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  for (const item of items) {
+    let {startAngle, endAngle} = item.angles;
+
+    startAngle = convertDegreesToRadians(startAngle);
+    endAngle = convertDegreesToRadians(endAngle);
+
+    drawSlice({
+      context: ctx,
+      sliceParameters: {
+        startAngle,
+        endAngle,
+        x: canvas.width / 2,
+        y: canvas.width / 2,
+        radius: canvas.width / 2,
+      },
+      options: {
+        color: 'transparent',
+      },
+      // onDraw: (slice: Path2D) => onDraw(slice, item),
+    });
+  }
+};
+
+export const getSlicesPath2D = (
+  canvas: HTMLCanvasElement,
+  items: AuctionSlotWithAngles[]
+) => {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const center = canvas.width / 2;
+
+  return items.reduce((acc, item) => {
+    const slice = new Path2D();
+
+    const x = center;
+    const y = center;
+    const radius = center;
+
+    const startAngle = convertDegreesToRadians(item.angles.startAngle);
+    const endAngle = convertDegreesToRadians(item.angles.endAngle);
+
+    slice.moveTo(x, y);
+    slice.arc(x, y, radius - ctx.lineWidth, startAngle, endAngle);
+    slice.closePath();
+    slice.moveTo(x, y);
+
+    acc.push({path: slice, slot: item});
+
+    return acc;
+  }, [] as Array<{path: Path2D; slot: AuctionSlotWithAngles}>);
+};
+
+export const drawSlicesItemsWithSelectedItem = (
+  canvas: HTMLCanvasElement,
+  selectedItem: AuctionSlotWithAngles
+) => {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const center = canvas.width / 2;
+
+  drawSlice({
+    context: ctx,
+    sliceParameters: {
+      x: center,
+      y: center,
+      radius: center,
+      startAngle: convertDegreesToRadians(selectedItem.angles.startAngle),
+      endAngle: convertDegreesToRadians(selectedItem.angles.endAngle),
+    },
+    options: {
+      text: selectedItem.name,
+      color: selectedItem.auctionColor,
+    },
+  });
+};
+
+export const getSliceInfo = (
+  canvas: HTMLCanvasElement,
+  items: AuctionSlotWithAngles[],
+  mouse: {
+    x: number;
+    y: number;
+  }
+) => {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const center = canvas.width / 2;
+
+  return items.forEach((item) =>
+    drawSlice({
+      context: ctx,
+      sliceParameters: {
+        x: center,
+        y: center,
+        radius: center,
+        startAngle: convertDegreesToRadians(item.angles.startAngle),
+        endAngle: convertDegreesToRadians(item.angles.endAngle),
+      },
+      options: {
+        text: item.name,
+        color: item.auctionColor,
+      },
+      onDraw: (slice) => {
+        if (ctx.isPointInPath(slice, mouse.x, mouse.y)) console.log(item);
+      },
+    })
+  );
+};
+
+export const getAnglesForItems = (
+  radius: number,
+  lots: AuctionSlot[] | null,
+  wheelMode?: WheelMode
+): AuctionSlotWithAngles[] | null => {
+  if (!lots || lots.length === 0) return null;
+
+  const newLots =
+    wheelMode === 'dropout' ? reverseLotsByValue(lots) : [...lots];
+
+  const sumItemsValue = newLots.reduce((acc, lot) => acc + lot['value'], 0);
+  const maxWheelArcLength = getMaxCircleLength(radius);
+
+  let startAngle = 0;
+
+  return newLots.reduce((acc: AuctionSlotWithAngles[], item: AuctionSlot) => {
+    const itemArcLength =
+      maxWheelArcLength * getPercentValue(sumItemsValue, item['value']);
+
+    const degrees = getDegreeByArcLength(radius, itemArcLength);
+    const endAngle = startAngle + degrees;
+
+    acc.push({
+      ...item,
+      angles: {
+        startAngle,
+        endAngle,
+      },
+    });
+
+    startAngle = endAngle;
+
+    return acc;
+  }, []);
+};
+
+export const getSlotNameOnSelector = (
+  currentRotateDegree: number,
+  lots: AuctionSlotWithAngles[],
+  selectorDegree: number = 270
+) => {
+  const rotateAngle =
+    currentRotateDegree >= 360
+      ? currentRotateDegree - 360 * Math.floor(currentRotateDegree / 360)
+      : currentRotateDegree;
+
+  for (const lot of lots) {
+    const startAngle =
+      lot.angles.startAngle + rotateAngle <= 360
+        ? lot.angles.startAngle + rotateAngle
+        : lot.angles.startAngle +
+          rotateAngle -
+          360 * Math.floor((lot.angles.startAngle + rotateAngle) / 360);
+    const endAngle =
+      lot.angles.endAngle + rotateAngle <= 360
+        ? lot.angles.endAngle + rotateAngle
+        : lot.angles.endAngle +
+          rotateAngle -
+          360 * Math.floor((lot.angles.endAngle + rotateAngle) / 360);
+
+    if (endAngle < startAngle && startAngle <= selectorDegree) {
+      return lot.name;
+    }
+    if (selectorDegree >= startAngle && endAngle >= selectorDegree) {
+      return lot.name;
+    }
+  }
+  return '';
+};
+
+export const generateWinner = (
+  slots: AuctionSlotWithAngles[]
+): AuctionSlotWithAngles | null => {
+  const winnerRadians = 2 * Math.PI * Math.random();
+
+  for (const slot of slots) {
+    const {startAngle, endAngle} = slot.angles;
+
+    const startAngleInRadians = convertDegreesToRadians(startAngle);
+    const endAngleInRadians = convertDegreesToRadians(endAngle);
+
+    if (
+      winnerRadians >= startAngleInRadians &&
+      endAngleInRadians >= winnerRadians
+    ) {
+      return slot;
+    }
+  }
+
+  return null;
+};
+
+export const calculateRotateWheelCSSValue = (
+  slotWithAngles: AuctionSlotWithAngles,
+  spinCount: number = 10,
+  selectorDegree: number = 270
+) => {
+  const {startAngle, endAngle} = slotWithAngles.angles;
+
+  let randomValueFromRange = null;
+
+  if (endAngle >= startAngle)
+    randomValueFromRange = Math.random() * (endAngle - startAngle) + startAngle;
+  else {
+    randomValueFromRange =
+      Math.random() >= 0.5
+        ? Math.random() * (360 - startAngle) + startAngle
+        : Math.random() * endAngle;
+  }
+
+  const rotateCSSValue =
+    360 * spinCount + (selectorDegree - randomValueFromRange);
+
+  return rotateCSSValue;
+};
+
+const reverseLotsByValue = (slots: AuctionSlot[]) => {
+  // const sumOfLots = [...slots].reduce((acc, curr) => (acc += curr.value), 0);
+
+  // return [...slots].map((slot) => {
+  //   const reversedValue = (1 - slot.value / sumOfLots) / (slots.length - 1);
+
+  //   console.log(reversedValue * sumOfLots);
+
+  //   return { ...slot, value: reversedValue * sumOfLots };
+  // });
+
+  const sortedLots = slots
+    .map((slot) => ({...slot}))
+    .sort((a, b) => a.value - b.value);
+
+  let leftPointer = 0;
+  let rightPointer = slots.length - 1;
+
+  while (leftPointer <= rightPointer) {
+    const buffer = sortedLots[leftPointer].value;
+    sortedLots[leftPointer].value = sortedLots[rightPointer].value;
+    sortedLots[rightPointer].value = buffer;
+
+    leftPointer++;
+    rightPointer--;
+  }
+
+  return slots.map((lot) => {
+    const findingLot = sortedLots.find((item) => item._id === lot._id);
+
+    if (findingLot) {
+      lot.value = findingLot.value;
+    }
+
+    return {...lot};
+  });
+};
+
+export const sliceMouse = ({
+  canvas,
+  slice,
+  item,
+}: {
+  canvas: HTMLCanvasElement;
+  slice: Path2D;
+  item: AuctionSlotWithAngles;
+}) => {
+  let activeItem: {
+    path2D: Path2D | null;
+    item: AuctionSlotWithAngles | null;
+  } = {
+    path2D: null,
+    item: null,
+  };
+
+  canvas.addEventListener('mousemove', (ev: MouseEvent) => {
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    const isPointInPath = ctx.isPointInPath(
+      slice,
+      ev.offsetX * window.devicePixelRatio,
+      ev.offsetY * window.devicePixelRatio
+    );
+    const isSameSlice = activeItem.item === item;
+
+    if (isPointInPath && !isSameSlice) {
+      clearCanvas(canvas);
+
+      activeItem = {item, path2D: slice};
+
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
+      ctx.fill(slice);
+    }
+
+    if (!isPointInPath && isSameSlice) {
+      activeItem.item === null;
+      clearCanvas(canvas);
+    }
+  });
+};
+
+export const updateSlotsAnglesByRotateValue = (
+  slots: AuctionSlotWithAngles[],
+  rotateValue: number
+) => {
+  const newSlots: AuctionSlotWithAngles[] = [];
+
+  for (const slot of slots) {
+    let {startAngle, endAngle} = slot.angles;
+
+    const realRotate = rotateValue - Math.floor(rotateValue / 360) * 360;
+
+    if (startAngle + rotateValue > 360)
+      startAngle =
+        startAngle +
+        realRotate -
+        Math.floor((startAngle + realRotate) / 360) * 360;
+    else {
+      startAngle += realRotate;
+    }
+    if (endAngle + rotateValue > 360)
+      endAngle =
+        endAngle + realRotate - Math.floor((endAngle + realRotate) / 360) * 360;
+    else {
+      endAngle += realRotate;
+    }
+
+    newSlots.push({...slot, angles: {startAngle, endAngle}});
+  }
+
+  return newSlots;
+};
