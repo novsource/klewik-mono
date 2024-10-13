556571586020d534563bb5cd9cd3d319c09aac32
novsource <evstafev.nik@yandex.ru>
2024-10-13 21:53:44 +0800

    feat(types): add types

---
+++ apps/streamers-dashboard/src/@types/auction.d.ts
@@ -0,0 +1,38 @@
+type Auction = {
+  _id: string;
+  ownerId: string;
+  title: string;
+  slots: AuctionSlot[];
+  participants: AuctionParticipant[];
+  wheelMode: WheelMode;
+  dropoutSlots: AuctionSlot[];
+  isAlive: boolean;
+  startDate: Date;
+};
+
+type AuctionSlot = {
+  _id: string;
+  name: string;
+  value: number;
+  tag: string;
+  auctionColor: string;
+  ownerId?: string;
+  sponsorsIds?: AuctionSlotSponsor[];
+};
+
+type AuctionSlotWithAngles = AuctionSlot & {
+  angles: {startAngle: number; endAngle: number};
+};
+
+type AuctionSlotSponsor = {
+  userId: string;
+  value: number;
+};
+
+type WheelMode = 'classic' | 'dropout';
+
+type AuctionParticipant = {
+  _id: string;
+  username: string;
+  value: number;
+};
---
+++ apps/streamers-dashboard/src/@types/wheel-canvas.d.ts
@@ -0,0 +1,30 @@
+type ResizeCanvasProperties = {
+  canvas: HTMLCanvasElement;
+  wheelSelector: HTMLCanvasElement;
+  wrapper: HTMLDivElement;
+};
+
+type ResizeCanvas = (property: ResizeCanvasProperties) => void;
+
+type DrawSliceOptions = {
+  text?: string;
+  color?: string;
+  textAngle?: number;
+};
+
+type SliceParameters = {
+  x: number;
+  y: number;
+  radius: number;
+  startAngle: number;
+  endAngle: number;
+};
+
+type DrawSliceProperties = {
+  context: CanvasRenderingContext2D;
+  sliceParameters: SliceParameters;
+  onDraw?(slice: Path2D): void;
+  options?: DrawSliceOptions;
+};
+
+type DrawSlice = (properties: DrawSliceProperties) => void;
