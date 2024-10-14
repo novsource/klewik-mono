5ead8d76d11ed069e6c42800f2ee4a4472130bc1
novsource <evstafev.nik@yandex.ru>
2024-10-14 17:17:28 +0800

    feat(icons): add new icons

--- apps/streamers-dashboard/src/shared/components/ui/icons/index.ts
+++ apps/streamers-dashboard/src/shared/components/ui/icons/index.ts
@@ -1,21 +1,25 @@
-import { default as Crown } from './Crown';
-import { default as Face } from './Face';
-import { default as Github } from './Github';
-import { default as Home } from './Home';
-import { default as Id } from './Id';
-import { default as Key } from './Key';
-import { default as List } from './List';
-import { default as Logo } from './Logo';
-import { default as Money } from './Money';
-import { default as Preferences } from './Preferences';
-import { default as ReturnArrow } from './ReturnArrow';
-import { default as ReturnArrow } from './ReturnArrow';
-import { default as Settings } from './Settings';
-import { default as Telegram } from './Telegram';
-import { default as Timer } from './Timer';
+import {default as Bill} from './Bill';
+import {default as CardSend} from './CardSend';
+import {default as Crown} from './Crown';
+import {default as Face} from './Face';
+import {default as Github} from './Github';
+import {default as Home} from './Home';
+import {default as Id} from './Id';
+import {default as Key} from './Key';
+import {default as List} from './List';
+import {default as Logo} from './Logo';
+import {default as Money} from './Money';
+import {default as MoneyBag} from './MoneyBag';
+import {default as Preferences} from './Preferences';
+import {default as ReturnArrow} from './ReturnArrow';
+import {default as Settings} from './Settings';
+import {default as Telegram} from './Telegram';
+import {default as Timer} from './Timer';
 
-    export const Icons = {
-      Crown,
+export const Icons = {
+  Bill,
+  CardSend,
+  Crown,
   Face,
   Github,
   Home,
@@ -24,11 +28,11 @@ import { default as Timer } from './Timer';
   List,
   Logo,
   Money,
+  MoneyBag,
   Preferences,
   ReturnArrow,
   ReturnArrow,
   Settings,
   Telegram,
-  Timer
-    };
-  
+  Timer,
+};
