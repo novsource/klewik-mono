3bde1c1c995969422844f6bd9fcecf426912b6c5
novsource <evstafev.nik@yandex.ru>
2024-10-19 16:14:20 +0800

    feat(header): add timer button to header

--- apps/streamers-dashboard/src/shared/components/ui/Header/Header.tsx
+++ apps/streamers-dashboard/src/shared/components/ui/Header/Header.tsx
@@ -1,12 +1,12 @@
 import {Icons} from '@ui/icons';
+import {PropsWithChildren} from 'react';
 
-const Header = () => {
+const Header = ({children}: PropsWithChildren) => {
   return (
     <header className="w-full h-16">
       <div className="px-4 h-full w-full flex justify-between items-center gap-x-4">
-        <div className="">
-          <Icons.Logo />
-        </div>
+        <Icons.Logo />
+        <div className="">{children}</div>
       </div>
     </header>
   );
--- apps/streamers-dashboard/src/shared/router/layouts/AuctionDashboardLayout.tsx
+++ apps/streamers-dashboard/src/shared/router/layouts/AuctionDashboardLayout.tsx
@@ -1,28 +1,24 @@
 import {Outlet} from 'react-router-dom';
 
 import {Header, DashboardMenu} from '@ui/index';
+import {Icons} from '@ui/icons';
 
 const AuctionDashboardLayout = () => {
   return (
     <>
-      <Header />
+      <Header>
+        <button className="rounded-pill h-10 bg-dark px-3 py-2 font-semibold text-gray-accent flex gap-x-1 items-center">
+          <Icons.Timer width={21} height={21} />
+          {time}
+        </button>
+      </Header>
       <main className="h-full w-full">
         <div className="px-2 w-full h-full">
           <div className="flex w-full h-full">
             <aside className="w-16 h-full flex-none">
               <DashboardMenu className="absolute top-1/2 -translate-y-1/2 left-2" />
             </aside>
-            <div className="h-full w-full px-4 flex-shrink-[2]">
-              {<Outlet />}
-            </div>
-            <div className="min-w-[300px] w-[500px] h-full py-5">
-              <div className="h-full flex flex-col gap-y-4">
-                <button className="bg-green text-white font-semibold text-title px-5 py-3 rounded-medium">
-                  Spin wheel
-                </button>
-                <div className="h-full bg-dark rounded-large"></div>
-              </div>
-            </div>
+            {<Outlet />}
           </div>
         </div>
       </main>
