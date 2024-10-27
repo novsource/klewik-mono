b61597f468f0d596234f4bcb37b690e65d3e3e3b
novsource <evstafev.nik@yandex.ru>
2024-10-27 14:39:27 +0800

    restyle(welcome page): change styles

--- apps/streamers-dashboard/src/pages/WelcomePage/WelcomePage.tsx
+++ apps/streamers-dashboard/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,27 +1,24 @@
 import { Icons } from '@ui/icons'
+import CloverAnimation from './components/CloverAnimation/CloverAnimation'
 
 const WelcomePage = () => {
   return (
     <>
       <main className="h-full w-full">
         <div className="flex h-full w-full flex-row">
-          <div className="h-full w-full min-w-[400px] max-w-[600px] basis-1/3">
-            <div className="h-full w-full px-10">
+          <div className="flex-shrink-1 container mx-auto h-full flex-grow xl:max-w-[550px]">
+            <div className="h-full w-full px-5 xl:px-8 2xl:px-10">
               <div className="flex h-full w-full flex-col justify-between">
                 <div className="mt-5">
-                  <button className="flex w-full items-center justify-center gap-x-2 rounded-medium bg-yellow bg-opacity-30 px-4 py-2 text-body font-medium text-yellow">
-                    <Icons.Face
-                      width={18}
-                      height={18}
-                      className="text-yellow"
-                    />
-                    Прочитать перед использованием
+                  <button className="flex w-full items-center justify-center gap-x-1 rounded-medium bg-yellow bg-opacity-40 px-4 py-2.5 text-body font-medium text-yellow hover:bg-opacity-30 md:text-nowrap xl:py-2">
+                    <Icons.Warning width={18} height={18} />
+                    Прочитать перед использованием!
                   </button>
                 </div>
-                <div className="mb-32 flex flex-col gap-y-6">
+                <div className="flex flex-col gap-y-6 after:mb-28 after:xl:mb-36 after:2xl:mb-40">
                   <Icons.Logo width={46} height={46} />
                   <div className="flex flex-col gap-y-2">
-                    <h1 className="text-[24px] font-bold leading-7">
+                    <h1 className="text-titleXL font-bold leading-5 2xl:text-[24px] 2xl:leading-7">
                       Добро пожаловать в поинтовый аукцион!
                     </h1>
                     <h4 className="text-body font-medium text-gray">
@@ -30,31 +27,32 @@ const WelcomePage = () => {
                   </div>
 
                   <div className="flex w-full flex-col gap-y-3">
-                    <button className="flex items-center justify-center gap-x-1 rounded-medium bg-green py-3 text-body font-medium transition-all hover:bg-opacity-80">
-                      <Icons.Login width={24} height={24} />
+                    <button className="flex items-center justify-center gap-x-1 rounded-medium bg-green py-2.5 text-body font-medium leading-4 transition-all hover:bg-opacity-70 xl:py-2.5 xl:text-body xl:leading-3">
+                      <Icons.Login width={21} height={21} />
                       Войти в аукцион
                     </button>
-                    <button className="flex items-center justify-center gap-x-1 rounded-medium bg-dark py-3 text-body font-medium leading-3 text-gray-accent transition-all hover:bg-opacity-80">
-                      <Icons.Plus width={24} height={24} />
+                    <button className="flex items-center justify-center gap-x-1 rounded-medium bg-dark py-2.5 text-body font-medium leading-4 text-gray-accent transition-all hover:bg-opacity-70 xl:py-2.5 xl:text-body xl:leading-3">
+                      <Icons.Plus width={21} height={21} />
                       Создать аукцион
                     </button>
                   </div>
                 </div>
-                <div className="mb-5">
-                  <a
-                    href="https://www.github.com"
-                    target="_blank"
-                    className="flex items-center gap-x-2 text-gray-accent transition-all hover:text-white"
-                  >
-                    <Icons.Github width={18} height={18} />
-                    <span className="text-body font-medium">Github</span>
-                  </a>
-                </div>
+
+                <a
+                  href="https://www.github.com"
+                  target="_blank"
+                  className="flex items-center gap-x-2 text-gray-accent transition-all after:mb-5 hover:text-white"
+                >
+                  <Icons.Github width={18} height={18} />
+                  <span className="hidden text-title font-medium 2xl:inline-block 2xl:text-body 2xl:font-medium">
+                    Github
+                  </span>
+                </a>
               </div>
             </div>
           </div>
-          <div className="h-full w-full flex-grow border-l-[1px] border-dark">
-            Animation
+          <div className="hidden h-full w-full flex-shrink-[2] flex-grow basis-2/3 border-l-[1px] border-dark bg-[#111] xl:block">
+            <CloverAnimation />
           </div>
         </div>
       </main>
--- apps/streamers-dashboard/src/shared/components/ui/icons/index.ts
+++ apps/streamers-dashboard/src/shared/components/ui/icons/index.ts
@@ -1,28 +1,29 @@
-import { default as Bill } from './Bill'
-import { default as CardSend } from './CardSend'
-import { default as Crown } from './Crown'
-import { default as Face } from './Face'
-import { default as Gamepad } from './Gamepad'
-import { default as Github } from './Github'
-import { default as Home } from './Home'
-import { default as Id } from './Id'
-import { default as Key } from './Key'
-import { default as List } from './List'
-import { default as Login } from './Login'
-import { default as Logo } from './Logo'
-import { default as Money } from './Money'
-import { default as MoneyBag } from './MoneyBag'
-import { default as Plus } from './Plus'
-import { default as Preferences } from './Preferences'
-import { default as Refresh } from './Refresh'
-import { default as ReturnArrow } from './ReturnArrow'
-import { default as Settings } from './Settings'
-import { default as Telegram } from './Telegram'
-import { default as Timer } from './Timer'
-import { default as Tuning } from './Tuning'
+import { default as Bill } from './Bill';
+import { default as CardSend } from './CardSend';
+import { default as Crown } from './Crown';
+import { default as Face } from './Face';
+import { default as Gamepad } from './Gamepad';
+import { default as Github } from './Github';
+import { default as Home } from './Home';
+import { default as Id } from './Id';
+import { default as Key } from './Key';
+import { default as List } from './List';
+import { default as Login } from './Login';
+import { default as Logo } from './Logo';
+import { default as Money } from './Money';
+import { default as MoneyBag } from './MoneyBag';
+import { default as Plus } from './Plus';
+import { default as Preferences } from './Preferences';
+import { default as Refresh } from './Refresh';
+import { default as ReturnArrow } from './ReturnArrow';
+import { default as Settings } from './Settings';
+import { default as Telegram } from './Telegram';
+import { default as Timer } from './Timer';
+import { default as Tuning } from './Tuning';
+import { default as Warning } from './Warning';
 
-export const Icons = {
-  Bill,
+    export const Icons = {
+      Bill,
   CardSend,
   Crown,
   Face,
@@ -44,4 +45,6 @@ export const Icons = {
   Telegram,
   Timer,
   Tuning,
-}
+  Warning
+    };
+  
