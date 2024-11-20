075b683f7ae4fa0582be174d2fdf09eb592aa6af
novsource <evstafev.nik@yandex.ru>
2024-11-20 22:48:46 +0800

    feat(icons): add new icons - bin, magic stick and palette

--- apps/streamers-dashboard/src/shared/components/ui/icons/index.ts
+++ apps/streamers-dashboard/src/shared/components/ui/icons/index.ts
@@ -1,34 +1,38 @@
-import { default as Bill } from './Bill';
-import { default as CardSend } from './CardSend';
-import { default as Copy } from './Copy';
-import { default as Crown } from './Crown';
-import { default as DonateMessage } from './DonateMessage';
-import { default as EyeClosed } from './EyeClosed';
-import { default as EyeOpen } from './EyeOpen';
-import { default as Face } from './Face';
-import { default as Gamepad } from './Gamepad';
-import { default as Github } from './Github';
-import { default as Home } from './Home';
-import { default as Id } from './Id';
-import { default as Key } from './Key';
-import { default as LinkArrow } from './LinkArrow';
-import { default as List } from './List';
-import { default as Login } from './Login';
-import { default as Logo } from './Logo';
-import { default as Money } from './Money';
-import { default as MoneyBag } from './MoneyBag';
-import { default as Plus } from './Plus';
-import { default as Preferences } from './Preferences';
-import { default as Refresh } from './Refresh';
-import { default as ReturnArrow } from './ReturnArrow';
-import { default as Settings } from './Settings';
-import { default as Telegram } from './Telegram';
-import { default as Timer } from './Timer';
-import { default as Tuning } from './Tuning';
-import { default as Warning } from './Warning';
+import { default as Bill } from './Bill'
+import { default as Bin } from './Bin'
+import { default as CardSend } from './CardSend'
+import { default as Copy } from './Copy'
+import { default as Crown } from './Crown'
+import { default as DonateMessage } from './DonateMessage'
+import { default as EyeClosed } from './EyeClosed'
+import { default as EyeOpen } from './EyeOpen'
+import { default as Face } from './Face'
+import { default as Gamepad } from './Gamepad'
+import { default as Github } from './Github'
+import { default as Home } from './Home'
+import { default as Id } from './Id'
+import { default as Key } from './Key'
+import { default as LinkArrow } from './LinkArrow'
+import { default as List } from './List'
+import { default as Login } from './Login'
+import { default as Logo } from './Logo'
+import { default as MagicStick } from './MagicStick'
+import { default as Money } from './Money'
+import { default as MoneyBag } from './MoneyBag'
+import { default as Palette } from './Palette'
+import { default as Plus } from './Plus'
+import { default as Preferences } from './Preferences'
+import { default as Refresh } from './Refresh'
+import { default as ReturnArrow } from './ReturnArrow'
+import { default as Settings } from './Settings'
+import { default as Telegram } from './Telegram'
+import { default as Timer } from './Timer'
+import { default as Tuning } from './Tuning'
+import { default as Warning } from './Warning'
 
-    export const Icons = {
-      Bill,
+export const Icons = {
+  Bill,
+  Bin,
   CardSend,
   Copy,
   Crown,
@@ -45,8 +49,10 @@ import { default as Warning } from './Warning';
   List,
   Login,
   Logo,
+  MagicStick,
   Money,
   MoneyBag,
+  Palette,
   Plus,
   Preferences,
   Refresh,
@@ -55,6 +61,5 @@ import { default as Warning } from './Warning';
   Telegram,
   Timer,
   Tuning,
-  Warning
-    };
-  
+  Warning,
+}
