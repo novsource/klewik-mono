32cd9db6a92d79ed8e4cc97b9a173a442852e4dc
novsource <evstafev.nik@yandex.ru>
2024-11-24 02:48:36 +0800

    refactor(ui): add button variants

--- apps/streamers-dashboard/src/shared/components/ui/Button/Button.tsx
+++ apps/streamers-dashboard/src/shared/components/ui/Button/Button.tsx
@@ -1,46 +1,11 @@
 import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
-import { cva, type VariantProps } from 'class-variance-authority'
-
 import { cn } from '@/lib/utils'
-
-const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-medium text-body font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 text-body select-none',
-  {
-    variants: {
-      variant: {
-        default: 'bg-dark text-gray-accent hover:bg-opacity-80',
-        action: 'bg-green text-white hover:bg-opacity-80',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-      },
-      isIconOnly: {
-        true: '',
-        false: '',
-      },
-      size: {
-        default: 'h-10 px-3 py-1 text-md leading-6 rounded-md',
-        sm: 'h-9 rounded-md px-2 text-sm leading-6',
-        lg: 'h-11 rounded-md px-3',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
-      isIconOnly: false,
-    },
-  }
-)
+import { ButtonVariantsProps, buttonVariants } from './ButtonVariants'
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+    Omit<ButtonVariantsProps, 'startContent' | 'endContent'> {
   asChild?: boolean
   startContent?: React.ReactNode
   endContent?: React.ReactNode
@@ -55,16 +20,23 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 
     const Comp = asChild ? Slot : 'button'
 
+    const style = React.useMemo(
+      () =>
+        cn(
+          buttonVariants({
+            variant,
+            size,
+            isIconOnly,
+            startContent: !!startContent,
+            endContent: !!endContent,
+          }),
+          className
+        ),
+      [isIconOnly, variant, size, startContent, endContent]
+    )
+
     return (
-      <Comp
-        className={cn(
-          buttonVariants({ variant, size, className, isIconOnly }),
-          (startContent || endContent) &&
-            'flex items-center justify-center gap-x-1'
-        )}
-        ref={ref}
-        {...otherProps}
-      >
+      <Comp className={style} ref={ref} {...otherProps}>
         {startContent}
         {!isIconOnly && children}
         {endContent}
---
+++ apps/streamers-dashboard/src/shared/components/ui/Button/ButtonVariants.ts
@@ -0,0 +1,81 @@
+import { CvaClassValue } from '@/shared/utils/types'
+import { VariantProps, cva } from 'class-variance-authority'
+
+type ButtonVariant =
+  | 'default'
+  | 'action'
+  | 'outline'
+  | 'destructive'
+  | 'secondary'
+  | 'ghost'
+  | 'link'
+
+type ButtonSize = 'sm' | 'default' | 'lg'
+
+type ButtonVariants = {
+  variant: {
+    [Variant in ButtonVariant]: CvaClassValue
+  }
+  size: {
+    [Size in ButtonSize]: CvaClassValue
+  }
+  isIconOnly: {
+    [Bool in 'true' | 'false']: CvaClassValue
+  }
+  startContent: {
+    [Bool in 'true' | 'false']: CvaClassValue
+  }
+  endContent: {
+    [Bool in 'true' | 'false']: CvaClassValue
+  }
+}
+
+export const buttonVariants = cva<ButtonVariants>(
+  [
+    'inline-flex items-center justify-center gap-2',
+    'ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+    'disabled:pointer-events-none disabled:opacity-50',
+    '[&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:pointer-events-none',
+    'whitespace-nowrap rounded-medium transition-colors select-none',
+  ],
+  {
+    variants: {
+      variant: {
+        default: 'bg-dark text-gray-accent hover:bg-opacity-80',
+        action: 'bg-green text-white hover:bg-opacity-80',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        outline:
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+        secondary:
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
+      },
+      isIconOnly: {
+        true: '',
+        false: '',
+      },
+      size: {
+        default: 'h-10 px-3 py-1 text-md leading-6 rounded-md font-medium',
+        sm: 'h-9 rounded-md px-2 text-sm leading-6 font-regular',
+        lg: 'h-11 rounded-md px-3 font-medium',
+      },
+      startContent: {
+        true: 'flex items-center justify-center gap-x-1',
+        false: '',
+      },
+      endContent: {
+        true: 'flex items-center justify-center gap-x-1',
+        false: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+      isIconOnly: false,
+    },
+  }
+)
+
+export type ButtonVariantsProps = VariantProps<typeof buttonVariants>
