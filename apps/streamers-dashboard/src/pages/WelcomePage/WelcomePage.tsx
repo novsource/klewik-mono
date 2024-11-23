ca9f15478a21cd1def2b26f2f0390e1e568cb5c4
novsource <evstafev.nik@yandex.ru>
2024-11-24 02:50:58 +0800

    refactor(ui): change html on component on welcome page

--- apps/streamers-dashboard/src/pages/WelcomePage/WelcomePage.tsx
+++ apps/streamers-dashboard/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,6 +1,6 @@
 import { Icons } from '@ui/icons'
 import CloverAnimation from './components/CloverAnimation/CloverAnimation'
-import { Slider } from '@ui/index'
+import { Button, Slider, Typography } from '@ui/index'
 import {
   SliderAdminContent,
   SliderCreateContent,
@@ -17,10 +17,12 @@ const WelcomePage = () => {
           <div className="h-full w-full px-5 xl:px-8 2xl:px-10">
             <div className="flex h-full w-full flex-col justify-between">
               <div className="mt-5">
-                <button className="flex w-full items-center justify-center gap-x-1 rounded-medium bg-yellow bg-opacity-40 px-4 py-2.5 text-body font-medium text-yellow hover:bg-opacity-30 md:text-nowrap xl:py-2">
-                  <Icons.Warning width={18} height={18} />
+                <Button
+                  className="w-full bg-yellow bg-opacity-40 text-yellow hover:bg-opacity-30 md:text-nowrap"
+                  startContent={<Icons.Warning width={18} height={18} />}
+                >
                   Прочитать перед использованием!
-                </button>
+                </Button>
               </div>
 
               <Slider
@@ -47,10 +49,13 @@ const WelcomePage = () => {
                 target="_blank"
                 className="mb-5 flex w-fit items-center gap-x-2 text-gray-accent transition-all hover:text-white"
               >
-                <Icons.Github width={24} height={24} />
-                <span className="hidden text-title 2xl:inline-block 2xl:text-body 2xl:font-medium">
+                <Icons.Github width={21} height={21} />
+                <Typography
+                  tag="span"
+                  className="2xl:text-md hidden 2xl:inline-block 2xl:font-medium"
+                >
                   Github
-                </span>
+                </Typography>
               </a>
             </div>
           </div>
