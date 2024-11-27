b2be47833fe7c96e68989da428cd14f2a1fd5dce
Poli Sour <evstafev.nik@yandex.ru>
2024-11-27 23:28:45 +0800

    restyle(ui): Table rows now reacts on hover

--- apps/streamers-dashboard/src/shared/components/ui/Table/TableVariants.ts
+++ apps/streamers-dashboard/src/shared/components/ui/Table/TableVariants.ts
@@ -2,14 +2,12 @@ import { cva } from 'class-variance-authority'
 
 export const tableBaseWrapperVariants = cva('relative w-full overflow-auto')
 
-export const tableBaseVariants = cva(
-  'w-full caption-bottom text-[15px] text-white'
-)
+export const tableBaseVariants = cva('w-full caption-bottom text-md text-white')
 
 export const tableHeaderVariants = cva('sticky [&_tr]:border-b')
 
 export const tableHeadVariants = cva(
-  'text-nowrap h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0'
+  'text-nowrap h-10 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0'
 )
 
 export const tableBodyVariants = cva('[&_tr:last-child]:border-0 px-4')
@@ -19,14 +17,14 @@ export const tableFooterVariants = cva(
 )
 
 export const tableRowVariants = cva(
-  'transition-colors data-[state=selected]:bg-muted'
+  'transition-colors data-[state=selected]:bg-muted [&>td]:hover:before:bg-dark-accent'
 )
 
 export const tableCellVariants = cva([
   'p-4 align-middle [&:has([role=checkbox])]:pr-0 relative',
   'text-nowrap overlow-hidden',
   'first:before:rounded-l-medium last:before:rounded-r-medium before:absolute',
-  'before:top-1/2 before:-translate-y-1/2 before:left-0 before:bg-dark before:-z-10 before:w-full data-[hovered=true]:before:bg-dark-accent',
+  'before:top-1/2 before:-translate-y-1/2 before:left-0 before:bg-dark before:-z-10 before:w-full data-[hovered=true]:before:bg-dark-accent before:transition-colors',
   'before:py-[22px]',
 ])
 
