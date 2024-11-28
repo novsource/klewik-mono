10d73b7e42d44ac20a541100340bffdb46038197
Poli Sour <evstafev.nik@yandex.ru>
2024-11-29 01:50:11 +0800

    feat(ui): add close icon to search input

--- apps/streamers-dashboard/src/pages/AuctionSlotsPage/components/SearchInput/SearchInput.tsx
+++ apps/streamers-dashboard/src/pages/AuctionSlotsPage/components/SearchInput/SearchInput.tsx
@@ -9,6 +9,13 @@ const SearchInput = (props: SearchInputProps) => {
       startContent={
         <Icons.Magnifier className="text-gray" width={16} height={16} />
       }
+      endContent={
+        <Icons.Close
+          className="cursor-pointer text-gray transition-colors hover:text-gray-accent"
+          width={24}
+          height={24}
+        />
+      }
       {...props}
       placeholder="Поиск по названию слота..."
     />
