35b7815ac87541b4137ac0bed1935ce280332199
Poli Sour <evstafev.nik@yandex.ru>
2024-11-29 01:51:37 +0800

    restyle(ui): change input styles

--- apps/streamers-dashboard/src/shared/components/ui/Input/InputVariants.ts
+++ apps/streamers-dashboard/src/shared/components/ui/Input/InputVariants.ts
@@ -71,7 +71,7 @@ export const inputVariants = cva<InputVariants>(
     'text-body font-medium text-white',
     'file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground',
     'disabled:cursor-not-allowed disabled:opacity-50',
-    'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-gray-accent',
+    'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:ring-gray',
     'rounded-medium ring-offset-background',
     'placeholder:text-gray',
   ],
