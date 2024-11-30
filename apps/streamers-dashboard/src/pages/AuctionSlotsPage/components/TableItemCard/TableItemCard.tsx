a81b4c00af0626e983084b7f54b092b02f021e7b
Poli Sour <evstafev.nik@yandex.ru>
2024-11-30 15:25:14 +0800

    feat(ui): add table item card

---
+++ apps/streamers-dashboard/src/pages/AuctionSlotsPage/components/TableItemCard/TableItemCard.tsx
@@ -0,0 +1,56 @@
+import AnimatedTruncText from '@/pages/AuctionWheelPage/components/WheelTabs/LotsWheelTab/LotCard/AnimatedTruncText'
+import { Card, CardContainer, CardContent, CardFooter } from '@ui/Card/Card'
+import { Icons } from '@ui/icons'
+import { Typography } from '@ui/index'
+
+type TableItemCardProps<T> = {
+  data: T
+}
+
+const TableItemCard = <T extends AuctionSlot>({
+  data,
+}: TableItemCardProps<T>) => {
+  return (
+    <CardContainer size="default">
+      <Card className="rounded-large">
+        <CardContent>
+          <div className="flex items-center gap-x-1">
+            <Typography
+              tag="span"
+              className="font-golosF text-nowrap text-gray-light"
+            >
+              Наименование слота:{' '}
+            </Typography>
+            <AnimatedTruncText>{data.name}</AnimatedTruncText>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Typography tag="span" className="font-golosF text-gray-light">
+              Количество очков:{' '}
+            </Typography>
+            <Typography tag="span" className="font-golosF">
+              {new Intl.NumberFormat('ru-ru').format(data.points)}
+            </Typography>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Typography tag="span" className="font-golosF text-gray-light">
+              Шанс:{' '}
+            </Typography>
+            <Typography tag="span" className="font-golosF">
+              {new Intl.NumberFormat('ru-ru').format(data.chance ?? 0)}%
+            </Typography>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex items-center gap-x-2">
+            <Icons.Id className="text-gray-accent" size="sm" />
+            <Typography tag="span" className="font-golosF">
+              #{data._id}
+            </Typography>
+          </div>
+        </CardFooter>
+      </Card>
+    </CardContainer>
+  )
+}
+
+export { TableItemCard }
