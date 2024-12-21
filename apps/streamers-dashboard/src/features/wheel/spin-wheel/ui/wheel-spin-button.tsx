471312776878b3fad1c9b2e1d5d681c092acb592
Poli Sour <evstafev.nik@yandex.ru>
2024-12-21 17:47:39 +0800

    feat(wheel): add spin wheel feature

---
+++ apps/streamers-dashboard/src/features/wheel/spin-wheel/ui/wheel-spin-button.tsx
@@ -0,0 +1,30 @@
+import { useCallback } from 'react'
+
+import { generateWinner } from '~widgets/wheel/utils/wheel-canvas'
+
+import { useStoreSelector } from '~shared/lib/redux-toolkit'
+import { Button } from '~shared/ui/button'
+import { Icons } from '~shared/ui/icons'
+
+const SpinWheelButton = () => {
+  const wheelEventBus = useStoreSelector((state) => state.wheel.emitter)
+  const wheelSlots = useStoreSelector((state) => state.wheel.slots)
+
+  const handleOnClick = useCallback(() => {
+    const winner = generateWinner(wheelSlots)
+
+    wheelEventBus.notify('spin', winner)
+  }, [wheelSlots])
+
+  return (
+    <Button
+      variant="action"
+      startContent={<Icons.Refresh size="default" />}
+      onClick={handleOnClick}
+    >
+      Прокрутить
+    </Button>
+  )
+}
+
+export default SpinWheelButton
