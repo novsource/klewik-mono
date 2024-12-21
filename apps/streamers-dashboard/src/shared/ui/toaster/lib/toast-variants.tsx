368d387d268758be254b0491e09a912bd930358e
Poli Sour <evstafev.nik@yandex.ru>
2024-12-22 02:15:59 +0800

    feat(toaster): sonner variants

---
+++ apps/streamers-dashboard/src/shared/ui/toaster/lib/toast-variants.tsx
@@ -0,0 +1,60 @@
+import { ExternalToast, toast } from 'sonner'
+
+import { Icons } from '~shared/ui/icons'
+import { Typography } from '~shared/ui/typograghy'
+
+export const toastBaseNotification = (
+  description: string,
+  toastOptions?: ExternalToast
+) => {
+  return toast('Уведомление', { description, ...toastOptions })
+}
+
+export const toastSuccessNotification = (
+  operationName: string,
+  toastOptions?: ExternalToast
+) => {
+  const successDescription = (
+    <div className="w-full flex items-center gap-x-1">
+      <div className="flex items-center justify-center h-5 w-5 bg-green/30 rounded-md">
+        <Icons.Warning className="text-green" width={12} height={12} />
+      </div>
+      <Typography className="text-sm text-green font-medium" tag="p">
+        Успешно
+      </Typography>
+    </div>
+  )
+  return toast(operationName, {
+    description: successDescription,
+    classNames: { content: 'gap-y-2' },
+    ...toastOptions,
+  })
+}
+
+export const toastErrorNotification = (
+  operationName: string,
+  reason: string,
+  toastOptions?: ExternalToast
+) => {
+  const errorDescription = (
+    <div className="w-full flex flex-col gap-y-2">
+      <Typography className="text-sm" tag="p">
+        Причина: {reason}
+      </Typography>
+      <div className="w-full flex items-center gap-x-1">
+        <div className="flex items-center justify-center h-5 w-5 bg-red/30 rounded-md">
+          <Icons.Close className="text-red" size="default" />
+        </div>
+        <Typography className="text-sm text-red font-medium" tag="p">
+          Ошибка
+        </Typography>
+      </div>
+    </div>
+  )
+
+  return toast(operationName, {
+    description: errorDescription,
+    duration: 10000,
+    ...toastOptions,
+  })
+}
--- apps/streamers-dashboard/src/shared/ui/toaster/styles/sonner-variants.ts
+++ apps/streamers-dashboard/src/shared/ui/toaster/styles/sonner-variants.ts
@@ -3,17 +3,15 @@ import { cva } from 'class-variance-authority'
 export const baseSonnerVariants = cva('toaster group')
 
 export const toastSonnerVariants = cva(
-  'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg'
+  'group toast group-[.toaster]:bg-[#222224] group-[.toaster]:text-white group-[.toaster]:border-1 group-[.toaster]:border-gray/70 group-[.toaster]:shadow-lg group-[.toaster]:rounded-large group-[.toaster]:text-lg'
 )
 
-export const descriptionSonnerVariants = cva(
-  'group-[.toast]:text-muted-foreground'
-)
+export const descriptionSonnerVariants = cva('group-[.toast]:text-gray')
 
 export const actionButtonSonnerVariants = cva(
-  'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground'
+  'group-[.toast]:bg-primary group-[.toast]:text-white group-[.toast]:rounded-pill'
 )
 
 export const cancelButtonSonnerVariants = cva(
-  'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground'
+  'group-[.toast]:bg-dark group-[.toast]:text-dark'
 )
