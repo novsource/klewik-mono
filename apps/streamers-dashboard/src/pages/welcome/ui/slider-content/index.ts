bacaaa6641e6b1b4de3531e979e113a6fc95ec8b
Poli Sour <evstafev.nik@yandex.ru>
2024-12-28 18:37:58 +0800

    refactor(pages): change slider contents names

--- apps/streamers-dashboard/src/pages/welcome/ui/slider-content/index.ts
+++ apps/streamers-dashboard/src/pages/welcome/ui/slider-content/index.ts
@@ -1,8 +1,8 @@
-import SliderAdminContent from './SliderAdminContent'
-import SliderRolesContent from './SliderRolesContent'
-import SliderSuccessContent from './SliderSuccessContent'
-import SliderWelcomeContent from './SliderWelcomeContent'
+import SliderAdminContent from './slider-admin-content'
 import SliderCreateContent from './slider-create-auction'
+import SliderRolesContent from './slider-roles-content'
+import SliderSuccessContent from './slider-success-content'
+import SliderWelcomeContent from './slider-welcome-content'
 
 export {
   SliderWelcomeContent,
