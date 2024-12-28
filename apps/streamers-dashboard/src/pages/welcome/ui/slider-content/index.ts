f2e4110c69a0484bce2aeb37f6cc5ce756a74e0d
Poli Sour <evstafev.nik@yandex.ru>
2024-12-28 18:50:46 +0800

    feat(pages): add new slider content - guest sign-in

--- apps/streamers-dashboard/src/pages/welcome/ui/slider-content/index.ts
+++ apps/streamers-dashboard/src/pages/welcome/ui/slider-content/index.ts
@@ -1,5 +1,6 @@
 import SliderAdminContent from './slider-admin-content'
 import SliderCreateContent from './slider-create-auction'
+import { SliderGuestContent } from './slider-guest-content'
 import SliderRolesContent from './slider-roles-content'
 import SliderSuccessContent from './slider-success-content'
 import SliderWelcomeContent from './slider-welcome-content'
@@ -10,4 +11,5 @@ export {
   SliderAdminContent,
   SliderCreateContent,
   SliderSuccessContent,
+  SliderGuestContent,
 }
