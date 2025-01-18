5c1fe56b72df916971af3277bec554506c4f7f0f
Poli Sour <evstafev.nik@yandex.ru>
2025-01-18 17:48:53 +0800

    fix: fix card

--- apps/streamers-dashboard/src/pages/auction-slots/ui/table-card-info/table-card-info.tsx
+++ apps/streamers-dashboard/src/pages/auction-slots/ui/table-card-info/table-card-info.tsx
@@ -1,58 +1,63 @@
 import { HTMLAttributes } from 'react'
 
-import {
-  Card,
-  CardContainer,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '~shared/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '~shared/ui/card'
 import { Icons } from '~shared/ui/icons'
 
 type TableCardInfoProps = HTMLAttributes<HTMLDivElement>
 
 const TableCardInfo = (props: TableCardInfoProps) => {
   return (
-    <CardContainer size="sm" variant="slots">
-      <div className="flex flex-nowrap gap-x-2 [&>section]:rounded-medium">
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-nowrap">
-              Подключенные платежные системы
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Icons.DonationAlerts width={18} height={24} />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-nowrap">Количество слотов</CardTitle>
-          </CardHeader>
-          <CardContent>0</CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-nowrap">
-              Общая сумма очков слотов
-            </CardTitle>
-          </CardHeader>
-          <CardContent>0</CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-nowrap">Свободные очки</CardTitle>
-          </CardHeader>
-          <CardContent>0</CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-nowrap">Очки стримера</CardTitle>
-          </CardHeader>
-          <CardContent>0</CardContent>
-        </Card>
-      </div>
-    </CardContainer>
+    <div className="flex flex-nowrap gap-x-2 [&>section]:rounded-medium">
+      <Card size="sm">
+        <CardHeader>
+          <CardTitle className="text-nowrap text-sm text-gray-light">
+            Подключенные платежные системы
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="pt-1">
+          <Icons.DonationAlerts width={18} height={24} />
+        </CardContent>
+      </Card>
+      <Card size="sm">
+        <CardTitle className="py-0 text-nowrap text-sm text-gray-light">
+          Количество слотов
+        </CardTitle>
+
+        <CardContent className="pt-1 font-semibold text-gray-accent">
+          0
+        </CardContent>
+      </Card>
+      <Card size="sm">
+        <CardHeader>
+          <CardTitle className="py-0 text-nowrap text-sm text-gray-light">
+            Общая сумма очков слотов
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="pt-1 font-semibold text-gray-accent">
+          0
+        </CardContent>
+      </Card>
+      <Card size="sm">
+        <CardHeader>
+          <CardTitle className="py-0 text-nowrap text-sm text-gray-light">
+            Свободные очки
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="pt-1 font-semibold text-gray-accent">
+          0
+        </CardContent>
+      </Card>
+      <Card size="sm">
+        <CardHeader>
+          <CardTitle className="py-0 text-nowrap text-sm text-gray-light">
+            Очки стримера
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="pt-1 font-semibold text-gray-accent">
+          0
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
