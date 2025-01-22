4286695404b51825b0e6186ae0144e1add58c7f5
Poli Sour <evstafev.nik@yandex.ru>
2025-01-22 22:46:27 +0800

    restyle(ui): typography sizes

--- apps/streamers-dashboard/src/shared/ui/typograghy/styles/typography-variants.ts
+++ apps/streamers-dashboard/src/shared/ui/typograghy/styles/typography-variants.ts
@@ -1,7 +1,8 @@
-import { CvaClassValue } from '@/shared/utils/types'
 import { VariantProps, cva } from 'class-variance-authority'
 
-import { TypographyTags } from './Typography'
+import { CvaClassValue } from '~shared/utils/types'
+
+import { TypographyTags } from '../ui/typography'
 
 type TypographyVariants = {
   tag: {
@@ -12,7 +13,7 @@ type TypographyVariants = {
 export const typographyVariants = cva<TypographyVariants>('', {
   variants: {
     tag: {
-      h1: 'text-titleXL font-bold leading-5 2xl:text-[24px] 2xl:leading-7',
+      h1: 'text-titleXL font-bold leading-5 desktopXL:text-[24px] desktopXL:leading-7',
       h2: 'text-title font-bold',
       h3: 'text-title font-semibold',
       h4: 'text-md font-semibold',
