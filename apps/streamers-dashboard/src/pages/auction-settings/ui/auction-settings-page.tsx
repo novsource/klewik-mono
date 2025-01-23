daf6bf659f73faa77f7b73c8e1774a3be4d72473
Poli Sour <evstafev.nik@yandex.ru>
2025-01-23 17:12:48 +0800

    feat(pages): add settings page

---
+++ apps/streamers-dashboard/src/pages/auction-settings/ui/auction-settings-page.tsx
@@ -0,0 +1,22 @@
+import { DonatePayIntegrationCard } from '~features/integrations/connect-integration/ui/donate-pay'
+import { DonationAlertsIntegrationCard } from '~features/integrations/connect-integration/ui/donation-alerts'
+
+import { Typography } from '~shared/ui/typograghy'
+
+const AuctionSettingsPage = () => {
+  return (
+    <div className="mb-4 flex gap-y-3 mobile:gap-y-5 tablet:gap-y-7 tablet:pb-4 tablet:px-2 max-w-[1500px] desktop:max-w-[1800px] desktopLg:max-w-[2100px] mx-auto w-full h-full">
+      <div className="flex flex-col h-full w-full gap-y-10">
+        <div className="flex flex-col gap-y-3 w-full h-fit">
+          <Typography tag="h2">Интеграции</Typography>
+          <div className="flex w-full items-center gap-x-2">
+            <DonationAlertsIntegrationCard />
+            <DonatePayIntegrationCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { AuctionSettingsPage }
--- apps/streamers-dashboard/src/shared/ui/typograghy/styles/typography-variants.ts
+++ apps/streamers-dashboard/src/shared/ui/typograghy/styles/typography-variants.ts
@@ -14,7 +14,7 @@ export const typographyVariants = cva<TypographyVariants>('', {
   variants: {
     tag: {
       h1: 'text-titleXL font-bold leading-5 desktopXL:text-[24px] desktopXL:leading-7',
-      h2: 'text-title font-bold',
+      h2: 'text-titleLg font-bold',
       h3: 'text-title font-semibold',
       h4: 'text-md font-semibold',
       span: 'text-md font-medium',
