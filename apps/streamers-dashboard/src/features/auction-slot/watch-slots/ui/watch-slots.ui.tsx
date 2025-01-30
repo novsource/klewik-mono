e429f9f397d12d36372d0763eedfa6613fc73875
Poli Sour <evstafev.nik@yandex.ru>
2025-01-30 17:20:13 +0800

    refactor(slots): change auction slot card

--- apps/streamers-dashboard/src/features/auction-slot/watch-slots/ui/watch-slots.ui.tsx
+++ apps/streamers-dashboard/src/features/auction-slot/watch-slots/ui/watch-slots.ui.tsx
@@ -1,4 +1,7 @@
-import { ComponentProps, memo } from 'react'
+import { ComponentProps, ReactNode, memo } from 'react'
+
+import { ClassValue } from 'clsx'
+import { motion } from 'framer-motion'
 
 import { AuctionSlot } from '~entities/auction-slot/model'
 import { auctionSlotsSelectors } from '~entities/auction-slot/store'
@@ -7,57 +10,96 @@ import { useStoreSelector } from '~shared/lib/redux-toolkit'
 
 import { AnimatedTruncText } from '~shared/ui/animated-trunc-text'
 import { Button } from '~shared/ui/button'
-import { Card, CardContent } from '~shared/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '~shared/ui/card'
 import { Icons } from '~shared/ui/icons'
 import { Typography } from '~shared/ui/typograghy'
+
+import { cn } from '~shared/utils'
+
+type AuctionCardChipProps = {
+  children: ReactNode
+  startContent?: JSX.Element
+  endContent?: JSX.Element
+  classNames?: {
+    base?: ClassValue
+    text?: ClassValue
+  }
+}
+
+const AuctionCardChip = (props: AuctionCardChipProps) => {
+  const { children, startContent, endContent, classNames } = props
+
+  return (
+    <div
+      className={cn(
+        'px-2 py-1 bg-gray/30 flex flex-row gap-x-1.5 items-center rounded-md',
+        classNames?.base
+      )}
+    >
+      {startContent}
+      <Typography
+        className={cn(
+          'font-golos-f text-md font-medium text-gray-accent',
+          classNames?.text
+        )}
+        tag="span"
+      >
+        {children}
+      </Typography>
+      {endContent}
+    </div>
+  )
+}
 
 const AuctionSlotCard = memo((props: AuctionSlot) => {
   const { id, name, points } = props
   return (
-    <Card className="flex flex-row justify-between">
-      <CardContent className="flex flex-col gap-y-2 pt-0">
-        <div className="flex flex-col gap-y-2">
-          <div className="flex flex-row gap-x-2 items-center">
-            <Typography className="text-gray-light text-nowrap" tag="p">
-              Название слота:
-            </Typography>
-            <AnimatedTruncText classNames="font-golos-f">
-              {name}
-            </AnimatedTruncText>
-          </div>
-          <div className="flex flex-row gap-x-2 items-center">
-            <Typography className="text-gray-light" tag="span">
-              Количество очков:
-            </Typography>
-            <Typography className="font-golos-f" tag="span">
-              {Intl.NumberFormat('ru-Ru').format(points).toString()}
-            </Typography>
-          </div>
-          <div className="flex flex-row gap-x-2 items-center">
-            <Typography className="text-gray-light" tag="span">
-              Шанс:
-            </Typography>
-            <Typography className="font-golos-f" tag="span">
-              10%
-            </Typography>
-          </div>
+    <Card className="flex flex-col justify-between border-1 border-dark gap-y-3 py-2">
+      <CardHeader className="flex items-start justify-between h-6">
+        <CardTitle className="w-full">
+          <AnimatedTruncText className="font-golos-f font-semibold">
+            {name}
+          </AnimatedTruncText>
+        </CardTitle>
+        <div className="flex flex-row gap-x-1 h-6">
+          <Button
+            variant={'ghost'}
+            isIconOnly
+            className="text-gray-accent transition-colors hover:text-white h-full px-1 py-1"
+            startContent={<Icons.Pencil />}
+            size={'sm'}
+          >
+            Изменить
+          </Button>
+          <Button
+            variant={'ghost'}
+            isIconOnly
+            className="text-gray-accent transition-colors hover:text-red h-full px-1 py-1"
+            startContent={<Icons.Bin />}
+            size={'sm'}
+          >
+            Удалить
+          </Button>
         </div>
-
-        <div className="flex flex-row gap-x-2 items-center">
-          <Icons.Id className="text-gray-light" />
-          <Typography className="font-golos-f" tag="span">
+      </CardHeader>
+      <CardContent className="w-full flex flex-col gap-y-2 pt-0">
+        <div className="w-full flex flex-row gap-x-2 items-center -ml-0.5">
+          <AuctionCardChip
+            startContent={<Icons.Id className="text-gray-light" size="sm" />}
+          >
             {id}
-          </Typography>
+          </AuctionCardChip>
+          <AuctionCardChip
+            startContent={<Icons.Coin className="text-gray-light" size="sm" />}
+          >
+            {Intl.NumberFormat('ru-Ru').format(points).toString()}
+          </AuctionCardChip>
+          <AuctionCardChip
+            classNames={{ base: 'bg-green/20', text: 'text-green' }}
+          >
+            30%
+          </AuctionCardChip>
         </div>
-      </CardContent>
-      <CardContent className="flex h-full pt-0">
-        <Button
-          className="text-red/90 transition-colors bg-red/10 hover:bg-red/20 hover:text-red"
-          startContent={<Icons.Bin size="sm" />}
-          size={'sm'}
-        >
-          Удалить
-        </Button>
       </CardContent>
     </Card>
   )
@@ -70,9 +112,14 @@ const AuctionSlotsList = (props: ComponentProps<'ul'>) => {
     <ul {...props}>
       {slots.map((slot) => {
         return (
-          <li key={slot.id}>
+          <motion.li
+            key={slot.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
             <AuctionSlotCard {...slot} />
-          </li>
+          </motion.li>
         )
       })}
     </ul>
