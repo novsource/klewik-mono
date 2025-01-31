b9a4834b1216ba325a9aec6290939e9284a0c648
Poli Sour <evstafev.nik@yandex.ru>
2025-01-31 18:22:37 +0800

    refactor(slots): change page slots statistics

--- apps/streamers-dashboard/src/pages/auction-slots/ui/auction-slots-page.tsx
+++ apps/streamers-dashboard/src/pages/auction-slots/ui/auction-slots-page.tsx
@@ -1,65 +1,68 @@
-import { tailwindScreens } from '~shared/constants/tailwindcss'
+import { CreateSlotsForm } from '~features/auction-slot/create-slots/ui'
+import { AuctionSlotsList } from '~features/auction-slot/watch-slots/ui'
+import { SlotsStatistics } from '~features/auction-slot/watch-statistics/ui'
+
 import { useMediaQuery } from '~shared/hooks/use-media-query'
+
 import { Button } from '~shared/ui/button'
 import { Icons } from '~shared/ui/icons'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '~shared/ui/sheet/ui/sheet'
+
+import { tailwindScreens } from '~shared/constants/tailwindcss'
 
 import SearchInput from './search-input/search-input'
-import SlotsTable from './slots-table/slots-table'
-import TableCardInfo from './table-card-info/table-card-info'
-import { TableItemCard } from './table-item-card/table-item-card'
 
-type AuctionSlotsPageProps = {}
-
-const items: AuctionSlot[] = Array(20).fill({
-  _id: '123',
-  name: Array(100).fill('Test').join(' '),
-  points: 1000,
-  chance: 10,
-})
-
-const AuctionSlotsPage = (props: AuctionSlotsPageProps) => {
+const AuctionSlotsPage = () => {
   const isMediaLargeThenTablet = useMediaQuery(
     `(min-width: ${tailwindScreens.tablet})`
   )
+
   return (
-    <div className="mb-4 grid h-full w-full grid-rows-slotsTable gap-y-3 mobile:gap-y-5 tablet:grid-rows-slotsDesktop tablet:gap-y-7 tablet:pb-4 tablet:pl-2">
-      <div className="flex flex-nowrap items-center gap-x-4 pt-1">
+    <div className="mb-4 grid grid-rows-slots-table gap-y-3 mobile:gap-y-5 tablet:grid-rows-slots-desktop tablet:max-w-[1100px] landtop:max-w-[1300px] tablet:gap-y-7 tablet:pb-4 tablet:px-2 desktop:max-w-[1800px] desktop-lg:max-w-[2100px] mx-auto w-full h-full">
+      {isMediaLargeThenTablet && (
+        <div className="w-full overflow-x-scroll">
+          <SlotsStatistics />
+        </div>
+      )}
+
+      <div className="flex flex-nowrap items-center justify-between gap-x-4 pt-1">
         <SearchInput size={!isMediaLargeThenTablet ? 'lg' : 'default'} />
         <div className="flex items-center gap-x-2">
-          <Button
-            size={!isMediaLargeThenTablet ? 'lg' : 'default'}
-            variant={'default'}
-            startContent={<Icons.Pencil size="sm" />}
-          >
-            {isMediaLargeThenTablet && 'Редактировать'}
-          </Button>
-          <Button
-            size={!isMediaLargeThenTablet ? 'lg' : 'default'}
-            variant={'action'}
-            startContent={<Icons.Plus size="sm" />}
-          >
-            {isMediaLargeThenTablet && 'Добавить слот'}
-          </Button>
+          <Sheet>
+            <SheetTrigger>
+              <Button
+                size={!isMediaLargeThenTablet ? 'lg' : 'default'}
+                variant={'action'}
+                startContent={<Icons.Plus size="xs" />}
+              >
+                {isMediaLargeThenTablet && 'Добавить слот'}
+              </Button>
+            </SheetTrigger>
+            <SheetContent side={isMediaLargeThenTablet ? 'right' : 'bottom'}>
+              <SheetHeader>
+                <SheetTitle>Добавление слота</SheetTitle>
+              </SheetHeader>
+              <SheetDescription className="mb-6 text-sm">
+                Здесь вы можете самостоятельно добавить слоты в аукцион. Очки,
+                которые будут указаны в добавленных слотах будут суммированны и
+                вычтены из "очки стримера"
+              </SheetDescription>
+              <CreateSlotsForm multiplySlots />
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
 
-      {isMediaLargeThenTablet && (
-        <div className="w-full overflow-x-scroll">
-          <TableCardInfo />
-        </div>
-      )}
-
-      {isMediaLargeThenTablet ? (
-        <SlotsTable />
-      ) : (
-        <div className="h-full w-full overflow-scroll">
-          <ul className="flex h-full w-full flex-col gap-y-2">
-            {items.map((item) => (
-              <TableItemCard data={item} />
-            ))}
-          </ul>
-        </div>
-      )}
+      <div className="h-full w-full overflow-scroll">
+        <AuctionSlotsList className="flex h-full w-full flex-col gap-y-2" />
+      </div>
     </div>
   )
 }
