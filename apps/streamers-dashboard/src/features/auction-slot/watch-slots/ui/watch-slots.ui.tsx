ea2b7e821c80b19fcf6ae753203cd77eae48e001
Poli Sour <evstafev.nik@yandex.ru>
2025-01-31 18:23:37 +0800

    refactor(slots): virtual slots list

--- apps/streamers-dashboard/src/features/auction-slot/watch-slots/ui/watch-slots.ui.tsx
+++ apps/streamers-dashboard/src/features/auction-slot/watch-slots/ui/watch-slots.ui.tsx
@@ -1,14 +1,14 @@
-import { ComponentProps, ReactNode, memo } from 'react'
+import { ReactNode, memo, useEffect, useMemo, useState } from 'react'
 
 import { ClassValue } from 'clsx'
 import { motion } from 'framer-motion'
+import VirtualList from 'rc-virtual-list'
 
 import { AuctionSlot } from '~entities/auction-slot/model'
 import { auctionSlotsSelectors } from '~entities/auction-slot/store'
 
 import { useStoreSelector } from '~shared/lib/redux-toolkit'
 
-import { AnimatedTruncText } from '~shared/ui/animated-trunc-text'
 import { Button } from '~shared/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '~shared/ui/card'
 import { Icons } from '~shared/ui/icons'
@@ -51,15 +51,22 @@ const AuctionCardChip = (props: AuctionCardChipProps) => {
   )
 }
 
-const AuctionSlotCard = memo((props: AuctionSlot) => {
-  const { id, name, points } = props
+type AuctionSlotCardProps = AuctionSlot & {
+  percent: string | number
+}
+
+const AuctionSlotCard = memo((props: AuctionSlotCardProps) => {
+  const { id, name, points, percent } = props
   return (
     <Card className="flex flex-col justify-between border-1 border-dark gap-y-3 py-2">
       <CardHeader className="flex items-start justify-between h-6">
         <CardTitle className="w-full">
-          <AnimatedTruncText className="font-golos-f font-semibold">
+          <Typography
+            tag="span"
+            className="font-golos-f text-title font-semibold"
+          >
             {name}
-          </AnimatedTruncText>
+          </Typography>
         </CardTitle>
         <div className="flex flex-row gap-x-1 h-6">
           <Button
@@ -97,7 +104,7 @@ const AuctionSlotCard = memo((props: AuctionSlot) => {
           <AuctionCardChip
             classNames={{ base: 'bg-green/20', text: 'text-green' }}
           >
-            30%
+            {percent}%
           </AuctionCardChip>
         </div>
       </CardContent>
@@ -105,26 +112,56 @@ const AuctionSlotCard = memo((props: AuctionSlot) => {
   )
 })
 
-const AuctionSlotsList = (props: ComponentProps<'ul'>) => {
-  const slots = useStoreSelector(auctionSlotsSelectors.getSlots)
+type AuctionSlotsListProps = {
+  data?: AuctionSlot[]
+  className?: ClassValue
+}
+
+const AuctionSlotsList = ({ data }: AuctionSlotsListProps) => {
+  const storedSlots = useStoreSelector(auctionSlotsSelectors.getSlots)
+  const [slots, setSlots] = useState(() => data ?? storedSlots)
+
+  useEffect(() => {
+    if (data === undefined) {
+      return setSlots(storedSlots)
+    }
+
+    setSlots(data)
+  }, [data, storedSlots])
+
+  const slotsWinPercents = useMemo(() => {
+    const slotsPointSum = slots.reduce((sum, slot) => sum + slot.points, 0)
+
+    return slots.reduce<string[]>((acc, slot) => {
+      acc.push(((slot.points / slotsPointSum) * 100).toFixed(1))
+      return acc
+    }, [])
+  }, [slots])
 
   return slots.length > 0 ? (
-    <ul {...props}>
-      {slots.map((slot) => {
+    <VirtualList
+      component={'ul'}
+      data={slots}
+      itemKey={'id'}
+      itemHeight={91}
+      fullHeight
+    >
+      {(slot, index) => {
         return (
           <motion.li
             key={slot.id}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
+            style={{ marginTop: index > 0 ? '8px' : '0' }}
           >
-            <AuctionSlotCard {...slot} />
+            <AuctionSlotCard {...slot} percent={slotsWinPercents[index]} />
           </motion.li>
         )
-      })}
-    </ul>
+      }}
+    </VirtualList>
   ) : (
-    <div className="flex flex-col gap-y-2 justify-center items-center">
+    <div className="flex flex-col gap-y-2 justify-center items-center h-full">
       <Icons.Logo className="text-gray" width={32} height={32} />
       <Typography tag="p" className="text-gray-light font-medium font-golos-f">
         Slots not found
