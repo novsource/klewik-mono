b960171fb8b5af36218cba2c6007a5a66b02c7e7
Poli Sour <evstafev.nik@yandex.ru>
2025-02-12 20:53:48 +0800

    restyle(auction): change search slots input

--- apps/viewers/src/app/(auction)/[auctionUUID]/components/search-slots-input/search-slots-input.ui.tsx
+++ apps/viewers/src/app/(auction)/[auctionUUID]/components/search-slots-input/search-slots-input.ui.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { MagnifierIcon } from "~/app/_shared/ui/icons";
 import { Input } from "~/app/_shared/ui/input";
 import { InputProps } from "~/app/_shared/ui/input/ui/input.ui";
 
@@ -8,8 +9,9 @@ type SearchSlotsInputProps = InputProps;
 export const SearchSlotsInput = (props: SearchSlotsInputProps) => {
   return (
     <Input
-      slotClassNames={{ base: "max-w-[400px]" }}
+      slotClassNames={{ base: "min-w-[300px] max-w-[400px] grow" }}
       placeholder="Поиск по названию слота..."
+      startContent={<MagnifierIcon className="text-gray" size="sm" />}
       {...props}
     />
   );
