d9f204c7189a096cb85e3d32711f70ace5969a3e
Poli Sour <evstafev.nik@yandex.ru>
2025-02-12 20:55:00 +0800

    restyle(auction): change integration chip

--- apps/viewers/src/app/(auction)/[auctionUUID]/components/integration-chip/integration-chip.ui.tsx
+++ apps/viewers/src/app/(auction)/[auctionUUID]/components/integration-chip/integration-chip.ui.tsx
@@ -14,17 +14,30 @@ export default function IntegrationPlatformChip({
 }) {
   const integrationLogo = {
     Twitch: (
-      <Image src="/twitchLogo.svg" alt="Twitch logo" width={14} height={14} />
+      <Image
+        src="/twitchLogo.svg"
+        alt="Twitch logo"
+        width={14}
+        height={14}
+        style={{ width: "14px", height: "14px" }}
+      />
     ),
     Youtube: (
-      <Image src="/youtube.svg" alt="Youtube logo" width={18} height={14} />
+      <Image
+        src="/youtube.svg"
+        alt="Youtube logo"
+        width={18}
+        height={18}
+        style={{ width: "18px", height: "18px" }}
+      />
     ),
     "Donation Alerts": (
       <Image
         src="/donationAlerts.svg"
         alt="Donation Alerts logo"
         width={14}
-        height={14}
+        height={18}
+        style={{ width: "14px", height: "18px" }}
       />
     ),
   }[integrationName];
