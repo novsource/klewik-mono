6cd3bce9b00ceb68054c108535d7ca385a4e5e5f
Poli Sour <evstafev.nik@yandex.ru>
2025-02-14 18:40:18 +0800

    feat(auction): title and search in header

--- apps/viewers/src/app/(auction)/[auctionUUID]/_widgets/controlled-slots/controlled-slots.ui.tsx
+++ apps/viewers/src/app/(auction)/[auctionUUID]/_widgets/controlled-slots/controlled-slots.ui.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { memo, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { Post, SlotsList } from "../../components/slots-list/";
 import { SearchSlotsInput } from "../../components/search-slots-input";
+import { useAppContext } from "~/app/_shared/context";
+import { useIntersection } from "~/app/_shared/hooks/use-intersection";
 
 type ControlledSlotsProps = {
   slots: Post[];
@@ -12,11 +14,28 @@ export const ControlledSlotsMemo = memo(function ControlledSlots(
   props: ControlledSlotsProps,
 ) {
   const [searchValue, setSearchValue] = useState<string>("");
+  const {
+    state: { searchBar },
+    dispatchers,
+  } = useAppContext();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const intersection = useIntersection(inputRef, { threshold: 0 });
+
+  useEffect(() => {
+    if (searchBar.entry !== intersection.entry) {
+      dispatchers?.searchBar(intersection);
+    }
+  }, [intersection, dispatchers, searchBar]);
 
   return (
     <div className="flex h-full flex-col gap-y-4 px-0.5 mb-4">
       <div className="flex w-full justify-between max-tablet:flex-col max-tablet:gap-y-1">
-        <SearchSlotsInput onChange={(e) => setSearchValue(e.target.value)} />
+        <SearchSlotsInput
+          ref={inputRef}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
       </div>
       <SlotsList slots={props.slots} filterTitle={searchValue} />
     </div>
--- apps/viewers/src/app/(auction)/[auctionUUID]/components/test-header/auction-header.tsx
+++ apps/viewers/src/app/(auction)/[auctionUUID]/components/test-header/auction-header.tsx
@@ -5,26 +5,54 @@ import { Header } from "~/app/_shared/ui/header";
 
 import { Typography } from "~/app/_shared/ui/typography";
 import { cn } from "~/app/_shared/utils";
+import { Button } from "~/app/_shared/ui/button";
+import { MagnifierIcon } from "~/app/_shared/ui/icons";
 
 const AuctionHeader = () => {
   const {
-    state: { integrations },
+    state: { searchBar, title },
   } = useAppContext();
 
   return (
     <Header
       className={cn("tablet:px-0 border-b-1")}
-      style={{ borderColor: `rgba(90,93,97, ${1 - integrations.ratio})` }}
+      style={{
+        borderColor: `rgba(52,55,60, ${1 - (title.entry?.intersectionRatio ?? 1)})`,
+      }}
     >
       <div className="h-full w-full">
-        <div className="flex h-full w-full items-center tablet:px-4">
-          <Typography
-            className="text-md font-semibold"
-            tag="span"
-            style={{ opacity: 1 - integrations.ratio }}
+        <div className="grid grid-cols-3 h-full w-full items-center tablet:px-2">
+          <div
+            className={cn("flex flex-col")}
+            style={{
+              opacity: 1 - (title.entry?.intersectionRatio ?? 1),
+            }}
           >
-            Аукцион №1000
-          </Typography>
+            <Typography
+              className="text-md font-semibold leading-4 text-nowrap"
+              tag="span"
+            >
+              Тестовый аукцион
+            </Typography>
+            <Typography
+              className="text-[10px] text-gray text-nowrap"
+              tag="span"
+            >
+              Cоздан: {new Intl.DateTimeFormat().format(Date.now())}
+            </Typography>
+          </div>
+          <Button
+            className={cn(
+              "hidden bg-dark text-gray rounded-pill w-full font-medium gap-x-2 h-7 [&_svg]:size-3 hover:text-gray-light hover:bg-dark-accent/60",
+              !searchBar.inView &&
+                "inline-flex transition-all justify-self-center",
+            )}
+            startContent={<MagnifierIcon className="text-gray" />}
+            size="sm"
+            style={{ opacity: 1 - (searchBar.entry?.intersectionRatio ?? 1) }}
+          >
+            Перейти к строке поиска{" "}
+          </Button>
         </div>
       </div>
     </Header>
