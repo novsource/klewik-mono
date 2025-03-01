99e560a435ae9c73293df18b25ae7a99fedf19b3
Poli Sour <evstafev.nik@yandex.ru>
2025-03-01 19:57:55 +0800

    refactor(lib): add axios auth base query

--- apps/streamers-dashboard/src/shared/lib/redux-toolkit/api.ts
+++ apps/streamers-dashboard/src/shared/lib/redux-toolkit/api.ts
@@ -1,48 +1,97 @@
 import { BaseQueryFn } from '@reduxjs/toolkit/query'
-import { AxiosError, AxiosRequestConfig } from 'axios'
+import {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponse,
+  CreateAxiosDefaults,
+} from 'axios'
+import { rateLimitOptions as RateLimitOptions } from 'axios-rate-limit'
 
-import type { BaseHttpClient } from '../axios'
+import { BaseHttpClient } from '../axios'
 
 type AxiosBaseQueryOptions = {
-  axiosInstance: BaseHttpClient
   baseUrl: string
+  axiosOptions?: CreateAxiosDefaults
+  rateLimiterOptions?: RateLimitOptions
 }
 
+type AxiosBaseQueryArgs = {
+  url: string
+  method?: AxiosRequestConfig['method']
+  data?: AxiosRequestConfig['data']
+  params?: AxiosRequestConfig['params']
+  headers?: AxiosRequestConfig['headers']
+}
+
+type AxiosBaseQueryResult = AxiosResponse<unknown, unknown>['data']
+
+type AxiosBaseQueryError = {
+  status: AxiosError['status']
+  data: AxiosError['response']
+}
+
+type AxiosQueryFn = BaseQueryFn<
+  AxiosBaseQueryArgs,
+  AxiosBaseQueryResult,
+  AxiosBaseQueryError
+>
+
 const axiosBaseQuery =
-  (
-    options: AxiosBaseQueryOptions
-  ): BaseQueryFn<
-    {
-      url: string
-      method?: AxiosRequestConfig['method']
-      data?: AxiosRequestConfig['data']
-      params?: AxiosRequestConfig['params']
-      headers?: AxiosRequestConfig['headers']
-    },
-    unknown,
-    unknown
-  > =>
-  async ({ url, method, data, params, headers }) => {
+  (options: AxiosBaseQueryOptions): AxiosQueryFn =>
+  /*@ts-ignore */
+  async ({ url, method, data, headers, params }) => {
     try {
-      const result = await options.axiosInstance.request(
-        options.baseUrl + url,
-        {
-          method,
-          data,
-          params,
-          headers,
-        }
-      )
+      const axios = new BaseHttpClient({
+        axiosOptions: options.axiosOptions,
+        rateLimiterOptions: options.rateLimiterOptions,
+      })
+
+      const result = await axios.request(options.baseUrl + url, {
+        method,
+        data,
+        params,
+        headers,
+      })
+
       return { data: result.data }
     } catch (axiosError) {
       const err = axiosError as AxiosError
+
       return {
         error: {
-          status: err.response?.status,
+          status: err.response?.status || 400,
           data: err.response?.data || err.message,
         },
       }
     }
   }
 
-export { axiosBaseQuery }
+const axiosAuthBaseQuery =
+  (options: AxiosBaseQueryOptions): AxiosQueryFn =>
+  async (args, api, extraOptions) => {
+    const baseQuery = axiosBaseQuery({
+      ...options,
+      baseUrl: import.meta.env.VITE_SERVER_URL + '/api',
+      axiosOptions: { withCredentials: true },
+      rateLimiterOptions: { maxRPS: 3 },
+    })
+
+    let result = await baseQuery({ url: args.url }, api, extraOptions)
+
+    if (result.error && result.error.status === 401) {
+      const refreshResult = await baseQuery(
+        { url: '/auth/refresh' },
+        api,
+        extraOptions
+      )
+
+      if (refreshResult.data) {
+        result = await baseQuery(args, api, extraOptions)
+      } else {
+        //TODO: Add method for logout user
+      }
+    }
+    return result
+  }
+
+export { axiosBaseQuery, axiosAuthBaseQuery }
