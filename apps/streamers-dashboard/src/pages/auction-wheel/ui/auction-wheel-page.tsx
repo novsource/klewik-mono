389849e216a0e24e4f70d310c81d1f0ae356ad60
Poli Sour <evstafev.nik@yandex.ru>
2025-03-04 15:10:03 +0800

    restyle: small style changes

--- apps/streamers-dashboard/src/pages/auction-wheel/ui/auction-wheel-page.tsx
+++ apps/streamers-dashboard/src/pages/auction-wheel/ui/auction-wheel-page.tsx
@@ -3,7 +3,7 @@ import { WheelContainer } from '~widgets/wheel/ui'
 
 const AuctionWheelPage = () => {
   return (
-    <div className="h-full desktop:max-w-[1800px] desktop-lg:max-w-[2100px] mx-auto">
+    <div className="h-full desktop:max-w-[1950px] desktop-lg:max-w-[2150px] mx-auto">
       <div className="flex h-full w-full flex-shrink-1">
         <WheelContainer />
         <div className="h-full basis-[30%] tablet:basis-[50%] landtop:basis-[40%] desktop-lg:basis-[35%] max-tablet:hidden">
