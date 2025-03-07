ea2dae2f27baa4092d606384cf945c7fef92dbe7
Poli Sour <evstafev.nik@yandex.ru>
2025-03-07 22:26:49 +0800

    feat(ui): add command, dialog and popover

---
+++ apps/streamers-dashboard/src/shared/ui/command/styles/command-variants.ts
@@ -0,0 +1,34 @@
+import { cva } from 'class-variance-authority'
+
+const commandVariants = cva(
+  'bg-popover text-popover-foreground flex h-full w-full flex-col overflow-hidden rounded-md'
+)
+
+const commandInputVariants = cva(
+  'placeholder:text-muted-foreground flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-hidden disabled:cursor-not-allowed disabled:opacity-50'
+)
+
+const commandListVariants = cva(
+  'max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto'
+)
+
+const commandGroupVariants = cva(
+  'text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium'
+)
+
+const commandItemVariants = cva(
+  "data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+)
+
+const commandShortcutVariants = cva(
+  'text-muted-foreground ml-auto text-xs tracking-widest'
+)
+
+export {
+  commandVariants,
+  commandInputVariants,
+  commandListVariants,
+  commandGroupVariants,
+  commandItemVariants,
+  commandShortcutVariants,
+}
