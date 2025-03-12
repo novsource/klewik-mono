ce1ec3fb13cd72c4a618f554888afc0a89ebed16
Poli Sour <evstafev.nik@yandex.ru>
2025-03-12 14:54:56 +0800

    feat(api): add get auction info api

--- apps/streamers-dashboard/src/entities/auction/api/auction.api.ts
+++ apps/streamers-dashboard/src/entities/auction/api/auction.api.ts
@@ -2,10 +2,22 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 
 import { axiosAuthBaseQuery } from '~shared/lib/redux-toolkit'
 
+import { Auction } from '../model'
+
+type GetAuctionInfoQueryArgs = {
+  auctionId: Auction['id']
+}
+
 const splittedAuctionApi = createApi({
   baseQuery: axiosAuthBaseQuery({ baseUrl: '/auction' }),
   reducerPath: 'auctionApi',
-  endpoints: () => ({}),
+  endpoints: (builder) => ({
+    getAuctionInfo: builder.query<Auction, GetAuctionInfoQueryArgs>({
+      query: ({ auctionId }) => ({ url: `/${auctionId}/info` }),
+    }),
+  }),
 })
 
 export { splittedAuctionApi }
+export const { useLazyGetAuctionInfoQuery, useGetAuctionInfoQuery } =
+  splittedAuctionApi
