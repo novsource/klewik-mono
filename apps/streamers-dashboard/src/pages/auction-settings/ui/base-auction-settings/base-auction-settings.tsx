a607fbe838c6a7350c03ba75c1cde5267a39ee85
Poli Sour <evstafev.nik@yandex.ru>
2025-03-28 16:14:00 +0800

    feat(settings): add delete auction

--- apps/streamers-dashboard/src/pages/auction-settings/ui/base-auction-settings/base-auction-settings.tsx
+++ apps/streamers-dashboard/src/pages/auction-settings/ui/base-auction-settings/base-auction-settings.tsx
@@ -1,7 +1,29 @@
+import { ChangeEvent, memo, useRef, useState } from 'react'
+
+import { useDeleteAuctionMutation } from '~pages/auction-settings/api/base-auction-settings'
+
+import { auctionSelectors } from '~entities/auction/store'
+
+import { useStoreSelector } from '~shared/lib/redux-toolkit'
+
 import { Button } from '~shared/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~shared/ui/dialog'
 import { Flex } from '~shared/ui/flex'
 import { Icons } from '~shared/ui/icons'
+import { Input } from '~shared/ui/input'
 import { TabsContent } from '~shared/ui/tabs'
+import { toastPromiseNotification } from '~shared/ui/toaster/lib'
+import { Typography } from '~shared/ui/typograghy'
+
+import { deleteAllSpacesFromString } from '~shared/utils/string-format'
 
 import { SettingsArea, SettingsAreasDivider } from '../auction-settings-area'
 
@@ -15,20 +37,89 @@ const BaseAuctionSettingsContent = () => {
     </TabsContent>
   )
 }
-const DeleteAuctionSettingsArea = () => {
+const DeleteAuctionSettingsArea = memo(() => {
+  const auctionId = useStoreSelector(auctionSelectors.getAuctionId)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const [isInputEmpty, setIsInputEmpty] = useState(true)
+  const [deleteAuctionMutation, { isLoading }] = useDeleteAuctionMutation()
+
+  const handleInputOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const clearInputValue = deleteAllSpacesFromString(e.target.value)
+
+    if (clearInputValue.length !== 0) {
+      setIsInputEmpty(false)
+    } else {
+      setIsInputEmpty(true)
+    }
+  }
+
+  const deleteAuction = async () => {
+    const request = deleteAuctionMutation({ auctionId })
+
+    toastPromiseNotification(request, 'Удаляем аукцион...', {
+      successText: 'Аукцион успешно удален!',
+      errorText: 'Не удалось удалить аукцион',
+    })
+  }
+
   return (
     <SettingsArea
       title="Удаление аукциона"
       description="Отправьте аукцион в небытие"
     >
-      <Button
-        className="bg-red/10 text-red/80 transition-all hover:bg-red/20 hover:text-red"
-        startContent={<Icons.Bin size="xs" />}
-      >
-        Удалить аукцион
-      </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            className="bg-red/10 text-red/80 transition-all hover:bg-red/20 hover:text-red"
+            startContent={<Icons.Bin size="xs" />}
+          >
+            Удалить аукцион
+          </Button>
+        </DialogTrigger>
+        <DialogContent
+          onInteractOutside={(e) => {
+            if (!isInputEmpty) e.preventDefault()
+          }}
+          slotsClassNames={{
+            close: 'cursor-pointer',
+            overlay: 'backdrop-blur-[2px] bg-dark-foreground/20',
+            content:
+              'w-full max-w-[500px] border-1 border-dark-accent rounded-[12px]',
+          }}
+        >
+          <DialogHeader>
+            <DialogTitle className="text-start">
+              Вы точно уверены что хотите удалить аукцион?
+            </DialogTitle>
+          </DialogHeader>
+          <DialogDescription></DialogDescription>
+          <Flex className="gap-y-2" direction="column">
+            <Typography tag="p">
+              Для подтверждения введите пароль от аукциона
+            </Typography>
+            <Input
+              ref={inputRef}
+              type="password"
+              placeholder="Введите пароль"
+              startContent={<Icons.Key className="text-gray-accent" />}
+              onChange={handleInputOnChange}
+            />
+          </Flex>
+          <DialogFooter className="flex flex-row">
+            <Button
+              className="w-full bg-red/10 text-red/80 transition-all hover:bg-red/20 hover:text-red"
+              startContent={<Icons.Bin size="xs" />}
+              onClick={deleteAuction}
+              disabled={isInputEmpty}
+            >
+              {!isLoading ? 'Удалить аукцион' : 'Удаляем аукцион...'}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </SettingsArea>
   )
-}
+})
 
 export { BaseAuctionSettingsContent }
--- apps/streamers-dashboard/src/shared/ui/dialog/ui/dialog.ui.tsx
+++ apps/streamers-dashboard/src/shared/ui/dialog/ui/dialog.ui.tsx
@@ -57,21 +57,32 @@ function DialogOverlay({
 }
 
 function DialogContent({
-  className,
+  slotsClassNames,
   children,
   ...props
-}: React.ComponentProps<typeof DialogPrimitive.Content> &
-  DialogContentVariantsProps) {
+}: Omit<React.ComponentProps<typeof DialogPrimitive.Content>, 'className'> &
+  DialogContentVariantsProps & {
+    slotsClassNames?: Partial<{
+      content: string
+      overlay: string
+      close: string
+    }>
+  }) {
   return (
     <DialogPortal data-slot="dialog-portal">
-      <DialogOverlay />
+      <DialogOverlay className={slotsClassNames?.overlay ?? ''} />
       <DialogPrimitive.Content
         data-slot="dialog-content"
-        className={cn(dialogContentVariants(), className)}
+        className={cn(dialogContentVariants(), slotsClassNames?.content)}
         {...props}
       >
         {children}
-        <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+        <DialogPrimitive.Close
+          className={cn(
+            "ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+            slotsClassNames?.close
+          )}
+        >
           <XIcon />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
