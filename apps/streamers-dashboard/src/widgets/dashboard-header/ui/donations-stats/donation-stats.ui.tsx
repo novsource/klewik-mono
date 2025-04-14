273cd57bd6cfb4f4ea11e541e2e12dad0b6b0b48
Poli Sour <evstafev.nik@yandex.ru>
2025-04-14 18:37:22 +0800

    feat(widgets): add base donations stats

---
+++ apps/streamers-dashboard/src/widgets/dashboard-header/ui/donations-stats/donation-stats.ui.tsx
@@ -0,0 +1,61 @@
+import { ReactNode, memo } from 'react'
+
+import NumberFlow from '@number-flow/react'
+
+import { Flex } from '~shared/ui/flex'
+import { Icons } from '~shared/ui/icons'
+
+import { cn } from '~shared/utils'
+
+const DonationsStats = memo(() => {
+  return (
+    <StatisticCard>
+      <Flex className="w-full h-full gap-x-2.5" align="center">
+        <Flex className="gap-x-1" align="center">
+          <Icons.Success size="sm" />
+          <NumberFlow
+            className="font-azeret-mono font-medium tracking-tight"
+            value={121}
+          />
+        </Flex>
+        <Flex className="gap-x-1" align="center">
+          <Icons.Warning width="16" height="16" />
+          <NumberFlow
+            className="font-azeret-mono font-medium tracking-tight"
+            value={10}
+          />
+        </Flex>
+        <Flex className="gap-x-0.5" align="center">
+          <Icons.LargeCross size="sm" />
+          <NumberFlow
+            className="font-azeret-mono font-medium tracking-tight"
+            value={0}
+          />
+        </Flex>
+      </Flex>
+    </StatisticCard>
+  )
+})
+
+const StatisticCard = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: ReactNode
+}) => {
+  return (
+    <Flex
+      className={cn(
+        'gap-x-1.5 py-1.5 px-2.5 h-9 bg-dark rounded-md text-md font-semibold leading-5 text-gray-accent',
+        className
+      )}
+      align="center"
+      justify="center"
+    >
+      {children}
+    </Flex>
+  )
+}
+
+export { DonationsStats }
