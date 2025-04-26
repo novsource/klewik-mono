7f8eb55832342e18890fac8bbc70295ae3e727e2
Poli Sour <evstafev.nik@yandex.ru>
2025-04-26 14:19:43 +0800

    feat(lib): add zod enum util

--- apps/streamers-dashboard/src/shared/lib/zod/index.ts
+++ apps/streamers-dashboard/src/shared/lib/zod/index.ts
@@ -1,4 +1,5 @@
 import { HexColorSchema, RGBColorSchema } from './color-custom.schema'
+import { zodEnum } from './enum-util'
 import { UnixTimestampInMsSchema } from './unix-timestamp-custom.schema'
 
-export { HexColorSchema, RGBColorSchema, UnixTimestampInMsSchema }
+export { HexColorSchema, RGBColorSchema, UnixTimestampInMsSchema, zodEnum }
