d610aca1626ba1765222bca600f47281df686258
Poli Sour <evstafev.nik@yandex.ru>
2025-04-30 17:37:07 +0800

    restyle(ui): skeleton bg color changes

--- apps/streamers-dashboard/src/shared/ui/skeleton/styles/skeletonVariants.ts
+++ apps/streamers-dashboard/src/shared/ui/skeleton/styles/skeletonVariants.ts
@@ -1,5 +1,5 @@
 import { cva } from 'class-variance-authority'
 
 export const skeletonVariants = cva(
-  'animate-pulse rounded-md bg-gray text-gray overflow-clip select-none'
+  'animate-pulse rounded-md bg-dark-accent text-gray overflow-clip select-none'
 )
