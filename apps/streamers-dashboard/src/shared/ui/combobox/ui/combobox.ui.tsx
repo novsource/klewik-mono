5a08f77f72cd6fb0dad8968f0a3ee6edb53e5c7f
Poli Sour <evstafev.nik@yandex.ru>
2025-06-18 20:05:54 +0800

    refactor(combobox): add few props

--- apps/streamers-dashboard/src/shared/ui/combobox/ui/combobox.ui.tsx
+++ apps/streamers-dashboard/src/shared/ui/combobox/ui/combobox.ui.tsx
@@ -14,7 +14,6 @@ import {
   CommandItem,
   CommandList,
 } from '~shared/ui/command'
-import { Icons } from '~shared/ui/icons'
 import { Popover, PopoverContent, PopoverTrigger } from '~shared/ui/popover'
 
 import { cn } from '~shared/utils'
@@ -30,6 +29,12 @@ export type ComboboxData = Array<
 type ComboboxProps<T extends ComboboxData = ComboboxData> = {
   data: T
   defaultValue?: SlotsSortingOptions
+  emptyMessage?: string
+  triggerProps?: ButtonProps
+  slotClassnames?: {
+    contentWrapper?: string
+    contentItem?: string
+  }
   placeholder?: string
   enableSearchField?: boolean
   onValueChanged?: (label: T[number]['label']) => void
@@ -38,7 +43,16 @@ type ComboboxProps<T extends ComboboxData = ComboboxData> = {
 }
 
 export function Combobox(props: ComboboxProps) {
-  const { enableSearchField = false, placeholder, data, size, icon } = props
+  const {
+    enableSearchField = false,
+    placeholder,
+    data,
+    size,
+    icon,
+    emptyMessage,
+    slotClassnames,
+    triggerProps,
+  } = props
 
   const [open, setOpen] = useState(false)
   const [value, setValue] = useState(() => props.defaultValue ?? '')
@@ -60,26 +74,39 @@ export function Combobox(props: ComboboxProps) {
       <PopoverTrigger asChild>
         <Button
           variant="outline"
-          className="w-fit max-w-[400px] border-dark-accent/60 hover:border-dark-accent border-1 justify-start items-center gap-x-1.5 font-medium hover:text-white/80"
+          className={cn(
+            'w-fit max-w-[400px] border-dark-accent/60 hover:border-dark-accent border-1 justify-start items-center gap-x-1.5 font-medium hover:text-white/80',
+            triggerProps?.className
+          )}
           role="combobox"
           startContent={icon}
           size={size}
           aria-expanded={open}
+          {...triggerProps}
         >
           {value
             ? data.find((item) => item.value === value)?.label
             : placeholder}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-fit max-w-[400px] p-0" align="start">
+      <PopoverContent
+        className={cn(
+          'w-fit max-w-[400px] p-0',
+          slotClassnames?.contentWrapper
+        )}
+        align="start"
+      >
         <Command>
           {enableSearchField && <CommandInput placeholder={placeholder} />}
           <CommandList>
-            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandEmpty>{emptyMessage ?? 'Ничего не найдено'}</CommandEmpty>
             <CommandGroup>
               {data.map((item) => (
                 <CommandItem
-                  className="font-medium cursor-pointer"
+                  className={cn(
+                    'font-medium cursor-pointer',
+                    slotClassnames?.contentItem
+                  )}
                   key={item.value}
                   value={item.value}
                   onSelect={(currentValue: string) => {
