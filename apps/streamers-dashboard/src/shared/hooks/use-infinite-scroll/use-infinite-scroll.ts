5e6615891e938604702ffde5fde577b67ed35d12
Poli Sour <evstafev.nik@yandex.ru>
2025-06-24 00:28:18 +0800

    feat(infinity-scroll): add disable

--- apps/streamers-dashboard/src/shared/hooks/use-infinite-scroll/use-infinite-scroll.ts
+++ apps/streamers-dashboard/src/shared/hooks/use-infinite-scroll/use-infinite-scroll.ts
@@ -1,52 +1,57 @@
 /**
   The concept of how this hook works was taken from the "ahooks" library.
   Resource: https://ahooks.js.org/hooks/use-infinite-scroll/
-*/
+ */
 import { useCallback, useRef, useState } from 'react'
 
 type InfiniteScrollService<T> = (
-  scrollState: { currentPage: number; limit: number },
+  scrollState: { currentPage: number, limit: number },
   ...serviceArgs: unknown[]
 ) => Promise<{ list: T[] }>
 
 type InfiniteScrollReturn<T> = {
   state: {
-    data: T[]
+    fetchedData: T[]
     page: number
     pageLimit: number
     isPending: boolean
     isCanLoadMore: boolean
+    isDisabled: boolean
   }
   functions: {
     loadMore: (...args: Parameters<InfiniteScrollService<T>>) => Promise<void>
     reset: () => void
+    enable: () => void
+    disable: () => void
     updatePageLimit: (limit: number) => void
   }
 }
 
 type InfiniteScrollOptions<T> = {
-  initData?: T[]
+  externalDataSource?: T[]
   pageLimit: number
 }
 
 const useInfiniteScroll = <T>(
   serviceFn: InfiniteScrollService<T>,
-  options: InfiniteScrollOptions<T>
+  options: InfiniteScrollOptions<T>,
 ): InfiniteScrollReturn<T> => {
-  const [data, setData] = useState<
+  const [fetchedData, setFetchedData] = useState<
     Awaited<ReturnType<typeof serviceFn>>['list']
-  >(() => options?.initData ?? [])
+  >([])
 
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(1)
   const [pageLimit, setPageLimit] = useState(() => options.pageLimit)
   const [isCanLoadMore, setIsCanLoadMore] = useState(true)
   const [isPending, setIsPending] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(false)
 
   const internalIsPendingRef = useRef(false)
 
   const loadMore = useCallback(
     async (...args: Parameters<typeof serviceFn>) => {
-      if (internalIsPendingRef.current || !isCanLoadMore) return
+      if (internalIsPendingRef.current || !isCanLoadMore || isDisabled)
+        return
 
       internalIsPendingRef.current = true
       setIsPending(true)
@@ -54,26 +59,32 @@ const useInfiniteScroll = <T>(
       const newData = await serviceFn(...args)
 
       setIsPending(false)
-      setData((curr) => [...curr, ...newData.list])
-      setPage((currentPage) => currentPage + 1)
+      setFetchedData(curr => [...curr, ...newData.list])
+      setPage(currentPage => currentPage + 1)
       setIsCanLoadMore(newData.list.length > pageLimit)
 
       internalIsPendingRef.current = false
     },
-    [serviceFn, pageLimit]
+    [serviceFn, pageLimit, isCanLoadMore, isDisabled],
   )
 
   return {
     state: {
-      data,
+      fetchedData,
       page,
       pageLimit,
       isPending,
       isCanLoadMore,
+      isDisabled,
     },
     functions: {
       loadMore,
-      reset: () => setIsCanLoadMore(false),
+      enable: () => setIsDisabled(false),
+      disable: () => setIsDisabled(true),
+      reset: () => {
+        setIsCanLoadMore(true)
+        setPage(1)
+      },
       updatePageLimit: (limit: number) => setPageLimit(limit),
     },
   }
