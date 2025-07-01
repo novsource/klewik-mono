45dc2ad29cddf3996af5f78e185eaff374b7913e
Poli Sour <evstafev.nik@yandex.ru>
2025-07-01 22:38:59 +0800

    chore(wheel): move wheel to entitie

---
+++ apps/streamers-dashboard/src/entities/wheel/hooks/index.ts
@@ -0,0 +1,5 @@
+import { useWheelControl } from './use-wheel-control'
+import { useWheelInit } from './use-wheel-init'
+import { useWheelSelector } from './use-wheel-selector'
+
+export { useWheelControl, useWheelInit, useWheelSelector }
--- apps/streamers-dashboard/src/pages/auction-wheel/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import { useWheelControl } from './use-wheel-control'
-import { useWheelInit } from './use-wheel-init'
-import * as wheelCanvas from './wheel-canvas'
-
-export { wheelCanvas, useWheelInit, useWheelControl }
