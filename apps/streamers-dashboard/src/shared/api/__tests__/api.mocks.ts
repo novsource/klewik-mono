dc475754765cbfda83a019f9327fc1030dc7147c
Poli Sour <evstafev.nik@yandex.ru>
2025-07-11 19:10:37 +0800

    chore(tests): add mocked api handlers

---
+++ apps/streamers-dashboard/src/shared/api/__tests__/api.mocks.ts
@@ -0,0 +1,46 @@
+import type { AuctionDTO } from '../http/auction'
+import type { CreateAuctionResponse } from '../http/auction/auction.api'
+
+import { faker } from '@faker-js/faker'
+import { checkPostFormRequestHeaders } from '~root/tests/mock-server/helpers'
+import { http, HttpResponse } from 'msw'
+import { z } from 'zod'
+
+import { AUCTION_ENDPOINTS, AUTH_ENDPOINTS } from '~shared/constants/api/http'
+
+type GetAuctionInfoPathIds = {
+  auctionUUID: string
+}
+
+export const auctionApiMocksHandlers = [
+  http.post(AUCTION_ENDPOINTS.CREATE, ({ request }) => {
+    const requestHeadersResponse = checkPostFormRequestHeaders(request.headers)
+    if (requestHeadersResponse)
+      return requestHeadersResponse
+
+    return HttpResponse.json<CreateAuctionResponse>(
+      { auctionId: faker.string.uuid(), auctionOwnerId: faker.string.uuid(), url: faker.internet.url() },
+    )
+  }),
+  http.get<GetAuctionInfoPathIds>(`/api/v1/auctions/:auctionUUID`, () => {
+    return HttpResponse.json<AuctionDTO>(
+      { id: faker.number.int(), auctionUUID: faker.string.uuid(), ownerId: faker.string.uuid(), createAt: faker.date.future(), endedAt: faker.date.future(), dropoutSlotsIds: [], processedDonationsIds: [], isBetsClosed: false, isEnded: false, slotsIds: [], url: faker.internet.url(), wheelMode: 'classic', winnerSlotId: null },
+    )
+  }),
+] as const
+
+export const authApiMocksHandlers = [
+  http.post(AUTH_ENDPOINTS.LOGIN, () => {
+
+  }),
+  http.post(AUTH_ENDPOINTS.REFRESH, ({ cookies }) => {
+    const validatedRefreshTokenResult = z.string().uuid().safeParse(cookies?.refreshToken ?? '')
+
+    if (!validatedRefreshTokenResult.success)
+      return new HttpResponse(null, { status: 400 })
+
+    return new HttpResponse(null, { headers: {
+      'set-cookie': `refreshToken=${faker.string.uuid()}; accessToken=${faker.string.uuid()}; deviceId=${faker.string.uuid()}`,
+    }, status: 200 })
+  }),
+]
---
+++ apps/streamers-dashboard/src/shared/api/http/auction/auction-api.contracts.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const AuctionDTOSchema = z.object({
+  id: z.number(),
+  auctionUUID: z.string().uuid(),
+  ownerId: z.string().uuid(),
+  slotsIds: z.number().array(),
+  url: z.string().url(),
+  wheelMode: z.enum(['classic', 'dropout']),
+  processedDonationsIds: z.number().array().nullable(),
+  winnerSlotId: z.number().nullable(),
+  dropoutSlotsIds: z.number().array(),
+  isBetsClosed: z.boolean(),
+  isEnded: z.boolean(),
+  createAt: z.date(),
+  endedAt: z.date(),
+})
