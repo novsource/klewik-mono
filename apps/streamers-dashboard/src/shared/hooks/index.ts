1bb18d3bc9be0b3ddda1efe1983e1ffb2781e9c6
Poli Sour <evstafev.nik@yandex.ru>
2025-07-31 15:53:17 +0800

    refactor(hooks): change timer hook

---
+++ apps/streamers-dashboard/src/shared/hooks/index.ts
@@ -0,0 +1,3 @@
+export * from './use-did-update/use-did-update'
+export * from './use-isomorphic-layout-effect/use-isomorphic-layout-effect'
+export * from './use-timer/use-timer'
---
+++ apps/streamers-dashboard/src/shared/hooks/use-did-update/use-did-update.ts
@@ -0,0 +1,35 @@
+import type { DependencyList, EffectCallback } from 'react'
+import { useRef } from 'react'
+
+import { useIsomorphicLayoutEffect } from '../use-isomorphic-layout-effect/use-isomorphic-layout-effect'
+
+/**
+ * @name useDidUpdate
+ * @description – Hook that triggers the effect callback on updates
+ * @category Lifecycle
+ *
+ * @param {EffectCallback} effect The effect callback
+ * @param {DependencyList} [deps] The dependencies list for the effect
+ *
+ * @example
+ * useDidUpdate(() => console.log("effect runs on updates"), deps);
+ */
+export const useDidUpdate = (effect: EffectCallback, deps?: DependencyList) => {
+  const mounted = useRef(false)
+
+  useIsomorphicLayoutEffect(
+    () => () => {
+      mounted.current = false
+    },
+    [],
+  )
+
+  useIsomorphicLayoutEffect(() => {
+    if (mounted.current) {
+      return effect()
+    }
+
+    mounted.current = true
+    return undefined
+  }, deps)
+}
