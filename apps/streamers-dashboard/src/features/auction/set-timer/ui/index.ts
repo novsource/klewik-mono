7b0bafb82678c101b71a11eb3540f4a61d6170b5
Poli Sour <evstafev.nik@yandex.ru>
2025-08-02 20:52:27 +0800

    feat(ui): add timer

--- apps/streamers-dashboard/src/features/auction/set-timer/ui/index.ts
+++
@@ -1,3 +0,0 @@
-import { TimerButton } from './timer'
-
-export { TimerButton }
---
+++ apps/streamers-dashboard/src/shared/ui/timer/index.ts
@@ -0,0 +1,3 @@
+export * from './context/timer-context'
+export * from './hooks/use-timer'
+export * from './ui/timer'
