35a211423fc3f79fb9b65b09ac3b03df429a2905
Poli Sour <evstafev.nik@yandex.ru>
2025-08-03 14:01:51 +0800

    feat(motion-box): add animate presense prop

--- apps/streamers-dashboard/src/shared/ui/motion-box/ui/motion-box.tsx
+++ apps/streamers-dashboard/src/shared/ui/motion-box/ui/motion-box.tsx
@@ -1,9 +1,23 @@
-import type { HTMLMotionProps } from 'motion/react'
+import type { AnimatePresenceProps, HTMLMotionProps } from 'motion/react'
 
+import { AnimatePresence } from 'motion/react'
 import * as m from 'motion/react-m'
 
-export type MotionBoxProps = HTMLMotionProps<'div'>
+export type MotionBoxProps = HTMLMotionProps<'div'> & {
+  withAnimatePresense?: boolean
+  animatePresenseProps?: AnimatePresenceProps
+}
 
 export const MotionBox = (props: MotionBoxProps) => {
+  const { withAnimatePresense, animatePresenseProps, ...restProps } = props
+
+  if (withAnimatePresense) {
+    return (
+      <AnimatePresence {...animatePresenseProps}>
+        <m.div {...restProps} />
+      </AnimatePresence>
+    )
+  }
+
   return <m.div {...props} />
 }
