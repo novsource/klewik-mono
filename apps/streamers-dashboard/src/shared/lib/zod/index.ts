2e316f517388aaa2a2111db73d4573e9a1d23f70
Poli Sour <evstafev.nik@yandex.ru>
2025-08-22 18:17:59 +0800

    chore(utils): add new color utils

--- apps/streamers-dashboard/src/shared/lib/zod/index.ts
+++ apps/streamers-dashboard/src/shared/lib/zod/index.ts
@@ -1,5 +1,3 @@
-import { HexColorSchema, RGBColorSchema } from './color-custom.schema'
-import { zodEnum } from './enum-util'
-import { UnixTimestampInMsSchema } from './unix-timestamp-custom.schema'
-
-export { HexColorSchema, RGBColorSchema, UnixTimestampInMsSchema, zodEnum }
+export * from './color-custom.schema'
+export * from './enum-util'
+export * from './unix-timestamp-custom.schema'
--- apps/streamers-dashboard/src/shared/utils/colors.ts
+++ apps/streamers-dashboard/src/shared/utils/colors.ts
@@ -1,3 +1,56 @@
+import type { HexColor, RGBAColor } from '~shared/lib/zod'
+
+export const isHexColor = (str: string): str is HexColor => {
+  if (str.length > 7 || str.length < 2 || str[0] !== '#') {
+    return false
+  }
+
+  let isHexColor = true
+
+  const strSlice = str.slice(1, str.length);
+
+  [...strSlice].forEach((symbol) => {
+    const numSym = Number(symbol)
+    const isSymbolNumber = Number.isInteger(numSym)
+
+    if (!isSymbolNumber) {
+      const lowercasedSymbol = symbol.toLowerCase()
+      const symbolCode = lowercasedSymbol.charCodeAt(0)
+
+      if (symbolCode < 97 || symbolCode > 102) {
+        isHexColor = false
+      }
+    }
+  })
+
+  return isHexColor
+}
+
+export const isRGBAColor = (str: string): str is RGBAColor => {
+  const matchesArr = str.match(/\w\w/g)
+
+  if (!matchesArr)
+    return false
+
+  return true
+}
+
+export const hexToRgba = (hex: string, alpha = 1): NullablePossible<RGBAColor> => {
+  const isHexNotValid = !isHexColor(hex)
+
+  if (isHexNotValid)
+    return null
+
+  const matchesArr = hex.match(/\w\w/g)
+
+  if (!matchesArr)
+    return null
+
+  const [r, g, b] = matchesArr.map(x => Number.parseInt(x, 16))
+
+  return `rgba(${r},${g},${b},${alpha})` as RGBAColor
+}
+
 export const getRandomRGBColor = () => {
   return `rgb(${Math.random() * 255},${Math.random() * 255},${
     Math.random() * 255
