d596257dbc63f21847c25945e7b2fae98732815b
Poli Sour <evstafev.nik@yandex.ru>
2025-09-02 00:21:00 +0800

    chore(hooks): small hooks organization changes

--- apps/streamers-dashboard/src/shared/utils/string-format.ts
+++ apps/streamers-dashboard/src/shared/utils/string-format.ts
@@ -7,29 +7,23 @@ type StringNumberFormatOptions = {
 
 export const stringNumberFormat = (
   str: string,
-  options?: StringNumberFormatOptions
+  options?: StringNumberFormatOptions,
 ) => {
   try {
     const value = Number.parseInt(str, 10)
 
     return formatNumberToIntlString(value, options)
-  } catch (error) {
-    if (error instanceof Error) throw new Error(error.message)
+  }
+  catch (error) {
+    if (error instanceof Error)
+      throw new Error(error.message)
   }
 }
 
-export const isStringContainNotOnlyDigits = (str: string) => {
-  const pattern = /[^0-9]/gm
-
-  const regex = new RegExp(pattern)
-
-  return !!str.match(regex)
-}
-
 export const deleteAllSpacesFromString = (str: string) => {
-  return [...str].filter((sym) => !sym.match(/(?<=\s)/)).join('')
+  return [...str].filter(sym => !sym.match(/(?<=\s)/)).join('')
 }
 
 export const removeSpaceDuplicatingFromString = (str: string) => {
-  return str.replace(/\s\s+/g, ' ')
+  return str.replace(/\s{2,}/g, ' ')
 }
--- apps/streamers-dashboard/src/shared/utils/string.ts
+++
@@ -1,7 +0,0 @@
-const isStringEmpty = (str: string) => {
-  const trimmedString = str.trim()
-
-  return !trimmedString || trimmedString.length === 0
-}
-
-export { isStringEmpty }
