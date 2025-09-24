6c336397938597089d5b7cbd652c7326c6da49e8
Poli Sour <evstafev.nik@yandex.ru>
2025-09-24 23:03:15 +0800

    feat(hooks): add hash hook

--- apps/viewers/src/hooks/index.ts
+++ apps/viewers/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './use-intersection-observer/use-intersection-observer';
 export * from './use-ref-state/use-ref-state';
 export * from './use-isomorphic-layout-effect/use-isomorphic-layout-effect';
+export * from './use-hash/use-hash'
