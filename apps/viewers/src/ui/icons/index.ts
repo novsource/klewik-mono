9c3a9d0946ef3ee0e3a3644a904fdc38c08757ce
Poli Sour <evstafev.nik@yandex.ru>
2025-10-04 23:31:09 +0800

    chore(icons): add plus icon

--- apps/viewers/src/ui/icons/index.ts
+++ apps/viewers/src/ui/icons/index.ts
@@ -5,6 +5,7 @@ import Hamburger from './hamburger.icon'
 import Id from './id.icon'
 import LinkArrow from './link-arrow.icon'
 import KlewikLogo from './logo.icon'
+import Plus from './plus.icon'
 import Magnifier from './search-icon'
 import SlotCoin from './slot-coin.icon'
 import TwitchLogo from './twitch-logo.icon'
@@ -13,6 +14,7 @@ import YoutubeLogo from './youtube-logo.icon'
 export const Icons = {
 	Id,
 	Crown,
+	Plus,
 	SlotCoin,
 	Hamburger,
 	GithubLogo,
