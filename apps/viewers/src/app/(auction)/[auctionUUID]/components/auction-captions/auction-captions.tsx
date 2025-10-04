0aaadcf5b7a038ba3c338f7144ec05d68ed97deb
Poli Sour <evstafev.nik@yandex.ru>
2025-10-04 23:52:43 +0800

    feat(auction): add auction captions

---
+++ apps/viewers/src/app/(auction)/[auctionUUID]/components/auction-captions/auction-captions.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useState } from 'react'
+import { useTimer } from '~hooks/index'
+import { Caption } from '~ui/caption'
+import { Flex } from '~ui/flex'
+
+export type AuctionInfoProps = {
+	createAt: string
+	revalidateDate: number
+}
+
+export const AuctionCaptions = (props: AuctionInfoProps) => {
+	const { createAt, revalidateDate } = props
+
+	const [isShouldReloadPage, setIsShouldReloadPage] = useState(false)
+
+	useTimer(120 - Math.floor((Date.now() - revalidateDate) / 1000),	{
+		immediately: true,
+		onExpire: () => {
+			setIsShouldReloadPage(true)
+		},
+	})
+
+	return (
+		<Flex className="gap-x-1.5">
+			<Caption
+				className="w-fit"
+				size="sm"
+				variant={isShouldReloadPage ? 'warn' : 'default'}
+				title="Время актуальности данных"
+			>
+				{isShouldReloadPage ? 'Необходимо обновление страницы' : 'Данные актуальные'}
+			</Caption>
+			<Caption
+				className="w-fit"
+				variant="default"
+				size="sm"
+				title="Дата и время создания"
+			>
+				{createAt}
+			</Caption>
+		</Flex>
+	)
+}
--- apps/viewers/src/app/(auction)/[auctionUUID]/page.tsx
+++ apps/viewers/src/app/(auction)/[auctionUUID]/page.tsx
@@ -1,20 +1,19 @@
 import process from 'node:process'
 
 import { notFound } from 'next/navigation'
-import { Typography } from '~ui/typography'
-import { ControlledSlotsMemo } from './_widgets/controlled-slots/controlled-slots.ui'
-import { IntegrationsInfo } from './_widgets/integrations-info'
+import { Button } from '~ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '~ui/dialog'
+import { Flex } from '~ui/flex'
+import { Icons } from '~ui/icons'
+import { AuctionCaptions } from './components/auction-captions'
 import { AuctionTitle } from './components/auction-title'
-import { RefreshPageTimer } from './components/refresh--page-timer'
-
-import { AuctionHeader } from './components/test-header'
+import { FiltredSlotsList } from './components/slots-list/filtred-slots-list.ui'
 
 export const revalidate = 120
 export const dynamicParams = true
 
 async function getSlots(id: string) {
 	const headers = new Headers()
-
 	headers.append('Content-type', 'application/json')
 
 	const response = await fetch(
@@ -38,7 +37,6 @@ async function getSlots(id: string) {
 
 async function getAuctionInfo(id: string) {
 	const headers = new Headers()
-
 	headers.append('Content-type', 'application/json')
 
 	const response = await fetch(`${process.env.SERVER_API_URL}/auctions/${id}`, {
@@ -90,29 +88,43 @@ export default async function AuctionPage({
 	}).format(Date.now())
 
 	return (
-		<>
-			<AuctionHeader title={auctionInfo.id} createAt={date} />
-			<main className="main_auction">
-				<div className="container w-full h-full mx-auto">
-					<div className="flex flex-col w-full h-full gap-y-6 tablet:gap-y-8 pt-4 px-4">
-						<div className="flex flex-col gap-y-2.5 tablet:gap-y-4">
+		<main className="main_auction">
+			<div className="container w-full h-full mx-auto">
+				<Flex
+					className="w-full h-full gap-y-6 tablet:gap-y-8 pt-4 px-2 tablet:px-4"
+					direction="column"
+				>
+					<Flex
+						className="gap-y-4.5 tablet:gap-y-5"
+						direction="column"
+					>
+						<Flex className="gap-x-6 tablet:gap-x-8" justify="between" align="start">
 							<AuctionTitle title={auctionInfo.id} date={date} />
-							<div className="flex flex-col gap-y-2">
-								<IntegrationsInfo />
-								<Typography className="text-sm text-gray-accent" tag="span">
-									Сайт обновится через:
-									{' '}
-									<RefreshPageTimer startTime={Date.now()} value={120} />
-									{' '}
-									секунд
-								</Typography>
-							</div>
-						</div>
-						<ControlledSlotsMemo slots={slots} />
-					</div>
-				</div>
-			</main>
-		</>
-
+							<Dialog>
+								<DialogTrigger
+									nativeButton={false}
+									render={(
+										<Button
+											variant="action"
+											startContent={<Icons.Plus />}
+										>
+											Создать код
+										</Button>
+									)}
+								/>
+								<DialogContent>
+									Test
+								</DialogContent>
+							</Dialog>
+						</Flex>
+						<AuctionCaptions
+							createAt={date}
+							revalidateDate={Date.now()}
+						/>
+					</Flex>
+					<FiltredSlotsList slots={slots} />
+				</Flex>
+			</div>
+		</main>
 	)
 }
