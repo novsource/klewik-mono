dc89958c7e6c1a264c9852222f0c15f34fe9be50
Poli Sour <evstafev.nik@yandex.ru>
2026-03-04 19:43:37 +0800

    chore(icons): add icons

---
+++ apps/streamers-dashboard/src/shared/ui/icons/AltArrowLeft.tsx
@@ -0,0 +1,21 @@
+import type { SVGProps } from 'react'
+
+type Sizes = 'xs' | 'sm' | 'default' | 'lg'
+type IconsProps = SVGProps<SVGSVGElement> & {
+  size?: Sizes
+}
+const sizes: Record<Sizes, number> = {
+  xs: 16,
+  sm: 18,
+  default: 21,
+  lg: 24,
+}
+const SvgAltArrowLeft = (props: IconsProps) => {
+  props = {
+    ...props,
+    width: props.size ? sizes[props.size] : props.width ?? sizes.default,
+    height: props.size ? sizes[props.size] : props.height ?? sizes.default,
+  }
+  return <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m8.165 11.63 6.63-6.43C15.21 4.799 16 5.042 16 5.57v12.86c0 .528-.79.771-1.205.37l-6.63-6.43a.5.5 0 0 1 0-.74" /></svg>
+}
+export default SvgAltArrowLeft
---
+++ apps/streamers-dashboard/src/shared/ui/icons/AltArrowRight.tsx
@@ -0,0 +1,21 @@
+import type { SVGProps } from 'react'
+
+type Sizes = 'xs' | 'sm' | 'default' | 'lg'
+type IconsProps = SVGProps<SVGSVGElement> & {
+  size?: Sizes
+}
+const sizes: Record<Sizes, number> = {
+  xs: 16,
+  sm: 18,
+  default: 21,
+  lg: 24,
+}
+const SvgAltArrowRight = (props: IconsProps) => {
+  props = {
+    ...props,
+    width: props.size ? sizes[props.size] : props.width ?? sizes.default,
+    height: props.size ? sizes[props.size] : props.height ?? sizes.default,
+  }
+  return <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M15.835 11.63 9.205 5.2C8.79 4.799 8 5.042 8 5.57v12.86c0 .528.79.771 1.205.37l6.63-6.43a.5.5 0 0 0 0-.74" /></svg>
+}
+export default SvgAltArrowRight
